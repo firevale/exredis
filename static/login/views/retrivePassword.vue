7fac037a71905a9928e9326755f2621f31db4efd
zhangsq <zhangshiqing@firevale.com>
2016-11-18 17:02:09 +0800

    wip

--- static/login/views/retrivePassword.vue
+++ static/login/views/retrivePassword.vue
@@ -6,25 +6,52 @@
           <p>{{ $t('account.login_page.titleRetrive') }}</p>
           <icon name="times" scale="2" fill-color="#666"></icon>
         </div>
+        <p v-if="hasSentCode && receiverType == 'phone'">
+          {{ $t('account.retrive_page.sendPhoneCodeTipPre') }}
+          <span class="errors">{{ receiverName }}</span>
+          {{ $t('account.retrive_page.sendPhoneCodeTipEnd') }}
+        </p>
+        <p v-if="hasSentCode && receiverType == 'email'">
+          {{ $t('account.retrive_page.sendEmailCodeTipPre') }}
+          <span class="errors">{{ receiverName }}</span>
+          {{ $t('account.retrive_page.sendEmailCodeTipEnd') }}
+        </p>
         <div class="row-login">
-          <validity ref="username" field="username" :validators="{
+          <validity v-show="!hasSentCode" ref="username" field="username" :validators="{
                 required: {rule: true, message: $t('account.error.requireUserName')}, 
-                maxLength: {rule: 50, message: $t('account.error.userNameTooLong')},
+                maxlength: {rule: 50, message: $t('account.error.userNameTooLong')},
                 validateUserName: {rule: true, message: this.supportPhone? $t('account.error.userNameWrong'):$t('account.error.userNameEmailWrong') },
                 }">
             <input type="text" :placeholder="supportPhone? $t('account.login_page.userPlaceHolder'): $t('account.login_page.userOnlyEmailPlaceHolder')"
               v-model.trim="userName" autocomplete="off" name="user" @focusout="handleValidate" />
           </validity>
+          <validity v-show="hasSentCode" ref="confirmCode" field="confirmCode" :validators="{
+                required: {rule: true, message: $t('account.login_page.userPasswordConfirmPlaceHolder')}, 
+                maxlength: {rule: 6, message: $t('account.error.confirmWordDifferent')},
+                minlength: {rule: 6, message: $t('account.error.confirmWordDifferent')},
+                validateSendCode: {rule: true, message: $t('account.error.confirmWordDifferent')},
+                }">
+            <input type="text" :placeholder="$t('account.login_page.userPasswordConfirmPlaceHolder')"
+              v-model.trim="confirmWorldInput" autocomplete="off" name="user" @focusout="handleValidate" />
+          </validity>
+          <input type="button" :class="{'inputDisabled': hasSentCode}" :value="hasSentCode? timerNum :$t('account.login_page.btnSendverificationCode')"
+            style="flex: 0.5;" @click.prevent="onReport" />
           <div class="headerIcon">
-						<icon name="user-o" fill-color="#aaa"></icon>
-					</div>
-          <div class="clearTimes" @click="userName=''">
+            <icon v-show="!hasSentCode" name="user-o" fill-color="#aaa"></icon>
+            <icon v-show="hasSentCode" name="pencil-square-o" fill-color="#aaa"></icon>
+          </div>
+          <div class="clearTimes" @click="clearUserNameOrCode" style="right: 35%;">
             <icon name="times" fill-color="#aaa"></icon>
           </div>
         </div>
-        <p v-if="usernameInvalid" class="errors"><icon name="info-circle" fill-color="#ff3860"></icon>{{ usernameTip }}</p>
+        <p v-if="usernameInvalid" class="errors">
+          <icon name="info-circle" fill-color="#ff3860"></icon>{{ usernameTip }}</p>
+        <p v-else v-if="confirmCodeInvalid" class="errors">
+          <icon name="info-circle" fill-color="#ff3860"></icon>{{ confirmCodeTip }}</p>
         <div class="row-login">
-          <input type="button" :value="$t('account.login_page.btnSendverificationCode')" @click.prevent="onReport" />
+        </div>
+        <div class="row-login" v-show="hasSentCode">
+          <input type="button" :value="$t('account.retrive_page.nextStep')" @click.prevent="onNext" />
         </div>
         <div class="row-login">
           <router-link :to="{ name: 'login' }">{{ $t('account.login_page.btnSubmit') }}</router-link>
@@ -36,6 +63,7 @@
 <script>
   import Icon from 'vue-awesome/components/Icon.vue'
   import 'vue-awesome/icons/times'
+  import 'vue-awesome/icons/pencil-square-o'
   export default {
     created(){
       let res = document.querySelector('meta[name="phone-register-support"]').getAttribute('content')
@@ -53,14 +81,26 @@
         }else{
           return this.validateEmail
         }
-      }
+      },
+
+      validateSendCode: function(val){
+        return this.validatePhoneNumber?this.confirmWorldInput == this.phoneCodeSent:
+          this.validateEmail?this.confirmWorldInput == this.emailCodeSent: false
+      },
+
     },
 
     data: function(){
       return {
+        hasSentCode: false,
+        timerNum: 60,
         supportPhone: false,
         userName: '',
-        passWord: '',
+        phoneCodeSent: 'iuy876',
+        emailCodeSent: 'o2u8y5',
+        confirmWorldInput: '',
+        receiverName: '',
+        receiverType: ''
       }
     },
 
@@ -75,10 +115,24 @@
                && this.$validation.validationRetrive.username.invalid
       },
 
+      confirmCodeInvalid: function() {
+        return this.$validation.validationRetrive 
+               && this.$validation.validationRetrive.confirmCode
+               && this.$validation.validationRetrive.confirmCode.invalid
+      },
+
       usernameTip: function() {
         let res=''
         if(this.$refs.username.result.invalid){
           res = this.$refs.username.result.errors && this.$refs.username.result.errors[0].message
+        }
+        return res
+      },
+
+      confirmCodeTip: function() {
+        let res=''
+        if(this.$refs.confirmCode.result.invalid){
+          res = this.$refs.confirmCode.result.errors && this.$refs.confirmCode.result.errors[0].message
         }
         return res
       },
@@ -100,9 +154,63 @@
           
         })
       },
+      timerCount: function(){
+        if(this.hasSentCode && this.timerNum > 0){
+          this.timerNum--
+          setTimeout(this.timerCount,1000)
+        }else{
+          this.hasSentCode = false
+          this.timerNum = 60
+        }
+      },
+      clearUserNameOrCode: function(){
+        if(this.hasSentCode){
+           this.confirmWorldInput = ''
+           this.$refs.confirmCode.$el.focus()
+        }else{
+          this.userName = ''
+          this.$refs.username.$el.focus()
+        } 
+      },
 
       onReport: function(e){
-        if(this.$validation.validationRetrive.valid){
+        if(this.$validation.validationRetrive.username.valid && this.userName.length && !this.hasSentCode){
+          this.hasSentCode = true
+          this.receiverName = this.userName
+          this.validatePhoneNumber?this.receiverType = 'phone': this.receiverType = 'email'
+          setTimeout(this.timerCount,1000)
+        }
+        return false
+
+        if(this.$validation.validationRetrive.valid && this.userName.length){
+          let urlApi = ''
+          if(this.supportPhone && this.validatePhoneNumber){
+            urlApi='sendCodeToPhone'
+          }else if(this.validateEmail){
+            urlApi='sendCodeToEmail'
+          }
+          this.$http({
+              method: 'POST',
+              url: urlApi,
+              params: {
+                userName: this.userName
+              }
+            }).then(response => {
+						let result = response.json()
+						if (result.success) {
+							this.hasSentCode = true
+              setTimeout(this.timerCount,1000)
+              this.receiverName = this.userName
+              this.validatePhoneNumber?this.receiverType = 'phone': this.receiverType = 'email'
+						} else {
+							return Promise.reject('error')
+						}
+					})
+        }
+      },
+
+      onNext: function(e){
+        if(this.$validation.validationRetrive.valid && this.userName.length && this.confirmWorldInput){
           this.$http({
               method: 'POST',
               url: '',
@@ -112,7 +220,7 @@
 						if (result.success) {
 							
 						} else {
-							return Promise.reject('account.error.invalidPassword')
+							return Promise.reject('error')
 						}
 					})
         }

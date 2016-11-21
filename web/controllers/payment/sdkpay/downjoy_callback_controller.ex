05b39a26606355da692d1fa7a2cc0ed2becad3f5
xiaobin <xiaobin@firevale.com>
2016-11-21 12:18:51 +0800

    add sdk pay purchase callback controllers

---
+++ web/controllers/payment/sdkpay/downjoy_callback_controller.ex
@@ -0,0 +1,36 @@
+defmodule Acs.SdkPay.DownjoyCallbackController do
+  use     Acs.Web, :controller
+  require SDKDownjoy
+
+  def purchase_callback(%Plug.Conn{private: %{acs_app: %RedisApp{} = app}} = conn, 
+                         %{"ext" => order_id, "order" => trans_no, "money" => amount} = params) do
+    case app.sdk_bindings.downjoy do 
+      %{"app_secret" => app_secret}->
+        if SDKDownjoy.validate_payment(app_secret, params) do
+          case Repo.get(AppOrder, order_id) do 
+            order = %AppOrder{} ->
+              {:ok, order} = AppOrder.changeset(order, %{
+                status: AppOrder.Status.paid,
+                paid_at: :calendar.local_time |> NaiveDateTime.from_erl!,
+                transaction_id: "downjoy." <> trans_no, 
+                fee: round(String.to_float(amount) * 100)
+              }) |> Repo.update
+
+              PaymentHelper.notify_cp(order)
+              conn |> text("success")  
+           _ -> 
+              Logger.error "order is not found, params: #{inspect params, pretty: true}"
+              conn |> text("failure") 
+          end 
+        else 
+          Logger.error "verify downjoy payment signature failed, params: #{inspect params, pretty: true}"
+          conn |> text("failure")  
+        end
+      _ -> 
+        Logger.error "receive invalid downjoy payment notifications, params: #{inspect params, pretty: true}"
+        conn |> text("failure") 
+    end
+  end
+  def purchase_callback(conn, _), do: conn |> text("failure") 
+
+end
---
+++ web/controllers/payment/sdkpay/iyouxi_callback_controller.ex
@@ -0,0 +1,141 @@
+defmodule Acs.SdkPay.IYouxiCallbackController do
+  use     Acs.Web, :controller
+  require SDKIYouxi
+
+  plug    :set_xml_header
+
+  defp set_xml_header(conn, _options) do 
+    conn |> put_resp_header("content-type", "application/xml")
+  end
+
+  ##################################################################### IF1 CALLBACK  #####################################################################
+  def if1_callback(%Plug.Conn{private: %{acs_app: %RedisApp{} = app}} = conn, 
+                   %{"cp_order_id" => order_id, 
+                     "correlator" => correlator,
+                     "order_time" => _order_time} = params) do
+    case app.sdk_bindings.iyouxi do
+      %{"app_key" => app_key} ->
+        if SDKIYouxi.verify_signature_if1(app_key, params) do
+          case Repo.get(AppOrder, order_id) do
+            order = %AppOrder{} ->
+              fee = (order.price / 100) |> Float.to_string([decimals: 2, compact: true])
+
+              result = ~s(
+              <sms_pay_check_resp>
+
+                <cp_order_id>#{order_id}</cp_order_id>
+
+                <correlator>#{correlator}</correlator>
+
+                <game_account>#{order.sdk_user_id}</game_account>
+
+                <fee>#{fee}</fee>
+
+                <if_pay>0</if_pay>
+
+                <order_time>#{Timex.format!(Timex.local, "%Y%m%d%H%M%S", :strftime)}</order_time>
+
+              </sms_pay_check_resp>
+              )
+
+              conn |> text(result)
+
+            _ -> 
+              Logger.error "order is not found, params: #{inspect params, pretty: true}"
+              conn |> text("fail") 
+          end 
+        else #{verify signature failed}
+          Logger.error "verify iyouxi payment signature failed, params: #{inspect params, pretty: true}"
+          conn |> text("fail")  
+        end
+      _ -> #{client_id invalid}
+        Logger.error "invalid client config, params: #{inspect params, pretty: true}"
+        conn |> text("fail") 
+    end
+  end
+
+  def if1_callback(conn, params) do
+    Logger.error "receive invalid iyouxi notification, params: #{inspect params, pretty: true}"
+    conn |> text("fail") 
+  end
+
+  ##################################################################### IF2 CALLBACK  #####################################################################
+  def if2_callback(conn, %{"method" => "check"} = params) do 
+    if1_callback(conn, params)
+  end
+
+  def if2_callback(%Plug.Conn{private: %{acs_app: %RedisApp{} = app}} = conn, 
+                   %{"cp_order_id" => order_id, 
+                     "correlator" => correlator, 
+                     "result_code" => "00",
+                     "method" => "callback",
+                     "fee" => fee} = params) do
+
+    resp =  fn(result_code) ->
+              conn |> text(~s(
+                  <cp_notify_resp>
+
+                    <h_ret>#{result_code}</h_ret>
+
+                    <cp_order_id>#{order_id}</cp_order_id>
+
+                  </cp_notify_resp>
+                )) 
+            end
+
+    case app.sdk_bindings.iyouxi do
+      %{"app_key" => app_key} ->
+        if SDKIYouxi.verify_signature_if2(app_key, params) do
+          case Repo.get(AppOrder, order_id) do
+            order = %AppOrder{} ->
+              {price, _} = Float.parse(fee)
+
+              {:ok, order} = AppOrder.changeset(order, %{
+                status: AppOrder.Status.paid,
+                paid_at: :calendar.local_time |> NaiveDateTime.from_erl!,
+                transaction_id: "iyouxi." <> correlator, 
+                fee: round(price * 100)
+              }) |> Repo.update
+
+              PaymentHelper.notify_cp(order)
+              resp.(0)
+            _ -> 
+              Logger.error "order is not found, params: #{inspect params, pretty: true}"
+              resp.(3) 
+          end 
+        else 
+          Logger.error "verify iyouxi payment signature failed, params: #{inspect params, pretty: true}"
+          resp.(4)  
+        end
+
+      _ -> 
+        Logger.error "invalid client config, params: #{inspect params, pretty: true}"
+        resp.(5) 
+    end
+  end
+
+  def if2_callback(conn, %{"cp_order_id" => order_id} = params) do  
+    Logger.error "receive invalid iyouxi payment notifications, params: #{inspect params, pretty: true}"
+    conn |> text(~s(
+      <cp_notify_resp>
+
+        <h_ret>0</h_ret>
+
+        <cp_order_id>#{order_id}</cp_order_id>
+
+      </cp_notify_resp>
+    ))
+  end
+
+  def if2_callback(conn, params) do
+    Logger.error "receive invalid iyouxi payment notifications, params: #{inspect params, pretty: true}"
+    conn |> text(~s(
+      <cp_notify_resp>
+
+        <h_ret>0</h_ret>
+
+      </cp_notify_resp>
+    ))
+  end
+
+end

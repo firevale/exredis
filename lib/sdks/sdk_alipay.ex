f7341ed74340cd6da092328eabd086ae4d71ec3d
xiaobin <xiaobin@firevale.com>
2017-02-23 23:24:20 +0800

    wip

--- lib/sdks/sdk_alipay.ex
+++ lib/sdks/sdk_alipay.ex
@@ -2,6 +2,7 @@ defmodule SDKAlipay do
   require Logger
   import  SweetXml
   require Utils
+  use     LogAlias
   alias   Utils.Httpc
 
   @verify_gateway_ssl "https://mapi.alipay.com/gateway.do?service=notify_verify"
@@ -29,23 +30,23 @@ defmodule SDKAlipay do
     :transport => "http"
   }
 
-  def direct(out_trade_no, subject, total_fee, platform) do 
+  def direct(out_trade_no, subject, total_fee, platform) do
     merchant_url = String.replace(@merchant_url, "%{platform}", to_string(platform))
     callback_url = String.replace(@callback_url, "%{platform}", to_string(platform))
     do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, @notify_url)
   end
-  def direct(out_trade_no, subject, total_fee, platform, merchant_url) do 
+  def direct(out_trade_no, subject, total_fee, platform, merchant_url) do
     callback_url = String.replace(@callback_url, "%{platform}", to_string(platform))
     do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, @notify_url)
   end
-  def direct(out_trade_no, subject, total_fee, platform, merchant_url, callback_url) do 
+  def direct(out_trade_no, subject, total_fee, platform, merchant_url, callback_url) do
     do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, @notify_url)
   end
-  def direct(out_trade_no, subject, total_fee, platform, merchant_url, callback_url, notify_url) do 
+  def direct(out_trade_no, subject, total_fee, platform, merchant_url, callback_url, notify_url) do
     do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, notify_url)
   end
 
-  defp do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, notify_url) do 
+  defp do_direct(out_trade_no, subject, total_fee, merchant_url, callback_url, notify_url) do
     req_id = Utils.generate_token
 
     req_data = """
@@ -60,7 +61,7 @@ defmodule SDKAlipay do
       </direct_trade_create_req>
     """
 
-    Logger.info "alipay, request data: #{req_data}"
+    info "alipay, request data: #{req_data}"
 
     param_tokens = %{
       :service => @config[:service_direct],
@@ -75,38 +76,38 @@ defmodule SDKAlipay do
 
     response = Httpc.post_msg(@config[:gateway], params_with_sign(param_tokens))
 
-    if Httpc.success?(response) do 
-      Logger.info "alipay direct success, response: #{inspect response.body, pretty: true}"
+    if Httpc.success?(response) do
+      info "alipay direct success, response: #{inspect response.body, pretty: true}"
       {:ok, response.body}
     else
-      Logger.error "alipay direct failed, response: #{inspect response, pretty: true}"
+      error "alipay direct failed, response: #{inspect response, pretty: true}"
       {:error, nil}
     end
   end
 
-  def get_request_token(result) do 
+  def get_request_token(result) do
     query = URI.decode_query(result)
 
-    if query["res_data"] do 
-      res_data = case @config[:sign_type] do 
-                   "0001" -> 
+    if query["res_data"] do
+      res_data = case @config[:sign_type] do
+                   "0001" ->
                       key_file = Application.app_dir(:acs, @config[:private_key])
                       Utils.rsa_priv_decrypt(key_file, query["res_data"])
                    _ -> query["res_data"]
                  end
 
-      token = res_data |> xpath(~x"/direct_trade_create_res/request_token/text()[1]") 
+      token = res_data |> xpath(~x"/direct_trade_create_res/request_token/text()[1]")
                        |> to_string
-                       |> String.strip 
+                       |> String.strip
 
       {:ok, token}
     else
-      Logger.error "get_request_token, invalid result: #{inspect query}"
+      error "get_request_token, invalid result: #{inspect query}"
       {:error, :empty_res_data}
     end
   end
 
-  def auth_and_execute(request_token) do 
+  def auth_and_execute(request_token) do
     req_data = "<auth_and_execute_req><request_token>#{request_token}</request_token></auth_and_execute_req>"
 
     params = %{
@@ -117,7 +118,7 @@ defmodule SDKAlipay do
       :v => "2.0",
       :req_id => Utils.generate_token,
       :req_data => req_data,
-      :_input_charset => @config[:input_charset]    
+      :_input_charset => @config[:input_charset]
     }
 
     signed_params = params_with_sign(params)
@@ -126,93 +127,92 @@ defmodule SDKAlipay do
     @config[:gateway] <> "?" <> URI.encode_query(signed_params)
   end
 
-  def verify_notify(%{"notify_data" => notify_data, "sec_id" => sec_id} = params) do 
-    
-    notify_data = case sec_id do 
-                    "0001" ->
-                      key_file = Application.app_dir(:acs, @config[:private_key])
-                      Utils.rsa_priv_decrypt(key_file, notify_data)
-                    _ ->
-                      notify_data
-                  end
+  def verify_notify(%{"notify_data" => notify_data, "sec_id" => sec_id} = params) do
+    notify_data
+      = case sec_id do
+          "0001" ->
+            key_file = Application.app_dir(:acs, @config[:private_key])
+            Utils.rsa_priv_decrypt(key_file, notify_data)
+          _ -> notify_data
+        end
 
     params = Map.put(params, "notify_data", notify_data)
 
-    if verify_notify_sign(params) do 
-      notify_xml = notify_data
-                      |> xpath(~x"//notify",
-                            notify_id: ~x"./notify_id/text()[1]", 
-                            trade_no: ~x"./trade_no/text()[1]",
-                            out_trade_no: ~x"./out_trade_no/text()[1]",
-                            trade_status: ~x"./trade_status/text()[1]", 
-                            total_fee:  ~x"./total_fee/text()[1]")
-                      |> Enum.into(%{}, fn({k, v}) -> {k, (v |> to_string |> String.strip)} end)
+    if verify_notify_sign(params) do
+      notify =
+        notify_data
+          |> xpath(~x"//notify",
+                notify_id: ~x"./notify_id/text()[1]",
+                trade_no: ~x"./trade_no/text()[1]",
+                out_trade_no: ~x"./out_trade_no/text()[1]",
+                trade_status: ~x"./trade_status/text()[1]",
+                total_fee:  ~x"./total_fee/text()[1]")
+          |> Enum.into(%{}, fn({k, v}) -> {k, (v |> to_string |> String.strip)} end)
 
-
-      if not is_nil(notify_xml.notify_id) and verify_notify_id(notify_xml.notify_id) do 
-        {:ok, notify_xml}
-      else
-        Logger.error "invalid notify_data: #{inspect notify_xml}"
-        {:error, :invalid_notify_id}
+      cond do
+        is_nil(notify.notify_id) ->
+          error "notify id is nil in #{inspect notify_data}"
+          {:error, :invalid_notify_id}
+        is_valid_notify_id(notify.notify_id) ->
+          {:ok, notify}
+        true ->
+          error "invalid notify id: #{notify.notify_id}"
+          {:error, :invalid_notify_id}
       end
     else
-      Logger.error "verify alipay notify signature failed"
+      error "verify alipay notify signature failed"
       {:error, :invalid_sign}
     end
   end
 
-  defp verify_notify_sign(%{"sign" => sign, "sec_id" => sec_id} = params) do 
+  defp verify_notify_sign(%{"sign" => sign, "sec_id" => sec_id} = params) do
     param_string = Enum.map_join(~w(service v sec_id notify_data), "&", &("#{&1}=#{params[&1]}"))
 
-    cond do 
-      sec_id == "MD5" ->
+    case sec_id do
+      "MD5" ->
         sign == Utils.md5_sign("#{param_string}#{@config[:key]}")
-      sec_id == "RSA" or sec_id == "0001" ->
+      x when x in ["RSA", "0001"] ->
         key_file = Application.app_dir(:acs, @config[:alipay_public_key])
         Utils.rsa_public_verify(key_file, param_string, sign)
-      true ->
-        false 
+      _ ->
+        false
     end
   end
 
-  defp verify_notify_id(notify_id) do 
+  defp is_valid_notify_id(notify_id) do
     response = Httpc.get(@verify_gateway_ssl <> "&partner=#{@config[:partner_id]}&notify_id=#{notify_id}")
-
-    if Httpc.success?(response) do 
-      Regex.match?(~r/true$/, response.body)
-    else 
-      Logger.error "verify_notify_id failed, response #{inspect response, pretty: true}"
-      false
-    end
+    Httpc.success?(response) && Regex.match?(~r/true$/, response.body)
   end
 
-  defp params_with_sign(%{} = params) do 
+  defp params_with_sign(%{} = params) do
     filtered_params = Enum.reduce(params, %{}, fn({key, value}, result) ->
-      unless is_nil(value) or value == "" do 
+      unless is_nil(value) or value == "" do
         Map.put(result, key, value)
       else
         result
       end
     end)
 
-    param_string = filtered_params |> make_param_string
+    param_string = make_param_string(filtered_params)
 
-    sign = cond do  
-             params[:sec_id] == "MD5" -> 
-               Utils.md5_sign("#{param_string}#{@config[:key]}")
-             params[:sec_id] == "0001" or params[:sec_id] == "RSA" ->
-               key_file = Application.app_dir(:acs, @config[:private_key])
-               Utils.rsa_priv_sign(key_file, param_string)
-             true ->
-               Logger.error "invalid sec_id param"
-               "invald"
-           end 
+    sign = case params[:sec_id] do
+      "MD5" ->
+        Utils.md5_sign("#{param_string}#{@config[:key]}")
+      x when x in ["0001", "RSA"] ->
+        key_file = Application.app_dir(:acs, @config[:private_key])
+        Utils.rsa_priv_sign(key_file, param_string)
+      _ ->
+        "invald"
+    end
 
     Map.put(filtered_params, :sign, sign)
   end
 
-  defp make_param_string(%{} = params) do 
-    Enum.map_join(params, "&", fn({key, value}) -> "#{key}=#{value}" end)
+  defp make_param_string(%{} = params) do
+    params
+      |> Enum.reject(fn({k, v}) -> is_nil(v) or v == "" end)
+      |> Enum.map(fn({k, v}) -> "#{k}=#{v}" end)
+      |> Enum.join("&")
   end
-  
+
 end

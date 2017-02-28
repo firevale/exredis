51ffdeabb71693ae4726bf4852ab42a0c526bf7d
xiaobin <xiaobin@firevale.com>
2017-02-28 10:32:26 +0800

    wip

--- test/features/alipay_test.exs
+++ test/features/alipay_test.exs
@@ -99,9 +99,4 @@ defmodule Acs.AlipayTest do
          |> put_req_header("user-agent", "fvacwebview android")
   end
 
-
-  # test "GET /", %{conn: conn} do
-  #   conn = get conn, "/"
-  #   assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  # end
 end

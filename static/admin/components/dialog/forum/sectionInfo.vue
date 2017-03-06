c3f288db3995b8029405ca8f8d327bb9b5a3e7b6
zhongxb <zhongxb@firevale.com>
2017-03-06 11:57:42 +0800

    wit

---
+++ static/admin/components/dialog/forum/sectionInfo.vue
@@ -0,0 +1,98 @@
+<template>
+  <modal :visible="visible">
+    <div class="box">
+      <div class="has-text-centered" style="width: 100%; margin-bottom: 10px">
+        <h5 class="title is-5">{{ $t('admin.titles.editSectionInfo') }}</h5>
+      </div>
+      <validation name="section" @submit.prevent="handleSubmit">
+        <label class="label"> {{ $t('admin.forum.section.title') }}: </label>
+        <p class="control">
+          <input class="input" type="text" v-model.trim="section.title">
+        </p>
+
+        <label class="label"> {{ $t('admin.forum.section.sort') }}: </label>
+        <p class="control">
+          <input class="input" type="text" v-model.trim="section.sort">
+        </p>
+
+        <label class="label"> {{ $t('admin.forum.section.active') }}: </label>
+        <p class="control">
+          <input class="input" type="text" v-model.trim="section.active">
+        </p>
+
+        <div class="container has-text-centered" style="margin-top: 15px">
+          <a class="button is-primary" :class="{'is-loading': processing}" @click.prevent="handleSubmit">{{ $t('admin.submit') }}</a>
+        </div>
+      </validation>
+    </div>
+  </modal>
+</template>
+
+<script>
+  import {
+    Modal
+  } from 'vue-bulma-modal'
+
+  import {
+    openNotification,
+    processAjaxError
+  } from 'admin/miscellaneous'
+
+  export default {
+    props: {
+      visible: {
+        type: Boolean,
+        default: true
+      },
+      section: Object,
+      callback: Function,
+    },
+
+    data() {
+      return {
+        processing: false,
+      }
+    },
+
+    methods: {
+      handleSubmit() {
+        this.processing = true
+        this.$http.post('/admin_actions/update_section_info', {
+            section: this.section,
+          })
+          .then(response => response.json())
+          .then(result => {
+            this.processing = false
+            if (result.success) {
+              openNotification({
+                title: this.$t('admin.titles.updateSuccess'),
+                message: this.$t('admin.messages.sectionInfoUpdated', {
+                  sectionTitle: result.section.title
+                }),
+                type: 'success',
+                duration: 4500,
+                container: '.notifications',
+              })
+
+              if (this.callback) {
+                this.callback(result.section)
+              }
+              this.visible = false
+            } else {
+              return Promise.reject(result)
+            }
+          })
+          .catch(e => {
+            this.processing = false
+            this.visible = false
+
+            processAjaxError(e)
+          })
+      }
+    },
+
+    components: {
+      Modal,
+    },
+  }
+</script>
---
+++ static/admin/components/forum/basicInfoEditor.vue
@@ -0,0 +1,114 @@
+<template>
+  <validation name="basicInfo" @submit.prevent="handleSubmit">
+    <div class="columns is-multiline">
+      <div class="column is-8" >
+        <label class="label"> {{ $t('admin.label.forumId')}}: </label>
+        <p class="control">
+          <input class="input is-disabled" type="text" v-model.trim="forum.id">
+        </p>
+      </div>
+      <div class="column is-8" >
+        <label class="label"> {{ $t('admin.label.forumAppId')}}: </label>
+        <p class="control">
+          <input class="input is-disabled" type="text" v-model.trim="forum.app_id">
+        </p>
+      </div>
+      <div class="column is-8">
+        <label class="label"> {{ $t('admin.label.forumTitle')}}: </label>
+        <p class="control">
+          <input class="input" type="text" v-model.trim="forum.title">
+        </p>
+      </div>
+      <div class="column is-8">
+        <label class="label"> {{ $t('admin.label.forumActive')}}: </label>
+        <p class="control">
+          <span class="select">
+            <select v-model.trim="forum.active">
+              <option v-for="status in states" :value="status">{{$t('admin.status.' + status)}}</option>
+            </select>
+          </span>
+        </p>
+      </div>
+      <div class="column is-8">
+        <label class="label"> {{ $t('admin.label.forumCreatedAt')}}: </label>
+        <p class="control">
+          <input class="input is-disabled" type="text" v-model.trim="forum.created_at">
+        </p>
+      </div>
+    </div>
+    <div class="container has-text-centered" style="margin-top: 15px">
+      <a class="button is-primary" :class="{'is-loading': processing}" @click.prevent="handleSubmit">{{ $t('admin.submit') }}</a>
+    </div>
+  </validation>
+</template>
+
+<script>
+  import {
+    mapGetters,
+    mapActions
+  } from 'vuex'
+
+  import Vue from 'admin/vue-i18n'
+
+  import {
+    openNotification,
+    processAjaxError,
+  } from 'admin/miscellaneous'
+
+  export default {
+    props: {
+      forum: Object,
+    },
+
+    data() {
+      return {
+        processing: false,
+        states: ['TRUE', 'FALSE']
+      }
+    },
+
+    methods: {
+      ...mapActions([
+        'addForum'
+      ]),
+
+      handleSubmit: function() {
+        this.processing = true
+        this.$http.post('/admin_actions/update_forum_info', {
+            forum: this.forum
+          })
+          .then(response => response.json())
+          .then(result => {
+            this.processing = false
+            if (result.success) {
+              openNotification({
+                title: this.$t('admin.titles.updateSuccess'),
+                message: this.$t('admin.messages.forumInfoUpdated', {
+                  forumName: this.forum.title
+                }),
+                type: 'success',
+                duration: 4500,
+                container: '.notifications',
+              })
+
+              // if (result.forum) {
+              //   this.addForum(result.forum)
+              //   this.$nextTick(_ => {
+              //     this.$router.replace({
+              //       path: `/admin/forums/edit/${result.forum.id}`
+              //     })
+              //   })
+              // }
+            } else {
+              return Promise.reject(result.message)
+            }
+          })
+          .catch(e => {
+            this.processing = false
+            processAjaxError(e)
+          })
+      },
+    },
+
+  }
+</script>

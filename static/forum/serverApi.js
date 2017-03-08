486d03de11eba91951dac87b5aaf9e6dec54d491
zhongxb <zhongxb@firevale.com>
2017-03-08 15:38:11 +0800

    wip

--- static/forum/serverApi.js
+++ static/forum/serverApi.js
@@ -1,28 +1,20 @@
 export default {
-    install: function(Vue, options) {
-        Vue.prototype.$acs = {
-            async getPagedPost(page, records_per_page, order) {
-                let response = await Vue.http.post('/forum_actions/get_paged_post', {
-                    page,
-                    records_per_page,
-                    order
-                })
-                return await response.json()
-            },
+  install : function(Vue, options) {
+    Vue.prototype.$acs = {
+      async getPagedPost(page, records_per_page, order) {
+        let response = await Vue.http.post('/forum_actions/get_paged_post', {page, records_per_page, order,})
+        return await response.json()
+      },
 
-            async getForumInfo(forum_id) {
-                let response = await Vue.http.post('/forum_actions/get_forum_info', {
-                    forum_id
-                })
-                return await response.json()
-            },
+      async getForumInfo(forum_id) {
+        let response = await Vue.http.post('/forum_actions/get_forum_info', {forum_id})
+        return await response.json()
+      },
 
-            async addPost(forum_id) {
-                let response = await Vue.http.post('/forum_actions/add_post', {
-                    forum_id
-                })
-                return await response.json()
-            }
-        }
+      async addPost(forum_id) {
+        let response = await Vue.http.post('/forum_actions/add_post', {forum_id})
+        return await response.json()
+      },
     }
+  }
 }

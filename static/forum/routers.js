0d313d7370dedd3617efa83f2acd041357d0c185
xiaobin <xiaobin@firevale.com>
2017-03-13 14:45:32 +0800

    wip

--- static/forum/routers.js
+++ static/forum/routers.js
@@ -7,9 +7,8 @@ export default function(VueRouter) {
       {
         path: '/forum/error',
         name: 'error',
-        component: require('./views/error.vue'),
-      },
-      {
+        component: require('./views/error.vue')
+      }, {
         path: '/forum/:forumId',
         name: 'forum',
         component: require('./views/main.vue'),
@@ -17,37 +16,37 @@ export default function(VueRouter) {
           {
             path: 'index',
             name: 'postList',
-            component: require('./views/postList.vue'),
+            component: require('./views/postList.vue')
           }, {
             path: 'search',
             name: 'search',
-            component: require('./views/search.vue'),
+            component: require('./views/search.vue')
           }, {
             path: 'detail/:postId',
             name: 'detail',
             component: require('./views/postDetail.vue'),
           }, {
+            path: 'comment',
+            name: 'newComment',
+            component: require('./views/newComment.vue')
+          }, {
             path: 'newPost',
             name: 'newPost',
-            component: require('./views/newPost.vue'),
-          }, {
-            path: 'writeComment',
-            name: 'writeComment',
-            component: require('./views/writeComment.vue'),
+            component: require('./views/newPost.vue')
           }, {
             path: 'customerService',
             name: 'customerService',
-            component: require('./views/customerService.vue'),
+            component: require('./views/customerService.vue')
           }, {
             path: 'personalPage',
             name: 'personalPage',
-            component: require('./views/personalPage.vue'),
+            component: require('./views/personalPage.vue')
           }, {
             path: 'campaign',
             name: 'campaign',
-            component: require('./views/campaign.vue'),
+            component: require('./views/campaign.vue')
           },
-        ],
+        ]
       },
     ],
   });

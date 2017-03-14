17c3282ea5036d7a96bd5274f39a8de3f52fb384
xiaobin <xiaobin@firevale.com>
2017-03-14 16:39:18 +0800

    wip

--- static/forum/components/postDetailView.vue
+++ static/forum/components/postDetailView.vue
@@ -32,7 +32,7 @@
       <div class="content">
         <p class="post-content" v-html="postData.content">
         </p>
-        <div class="tile" style="margin-bottom: 0.5rem;">
+        <div class="tile" style="margin-bottom: 0.5rem; align-items: center">
           <span :class="postData.is_favorite ? 'icon-heart' : 'icon-heart-o'" class="icon image-icon is-clickable"
             style="margin-right: 0.5rem" @click="toggleFavorite"></span>
           <span class="is-grey is-clickable" @click="toggleFavorite">

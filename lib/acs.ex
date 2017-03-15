db3171ea6d3775b15bd970a25467188e2cd7b329
Minzz <zhumingzhen@firevale.com>
2017-03-15 15:50:00 +0800

    wip

--- lib/acs.ex
+++ lib/acs.ex
@@ -78,38 +78,37 @@ defmodule Acs do
         orders: orders_mapping
       })
     end
+
+    unless Elasticsearch.is_index?("forum") do
+         settings = %{
+           number_of_shards: 5,
+           number_of_replicas: 1,
+         }
+
+         posts_mapping = %{
+           properties: %{
+             id: %{type: :integer},
+             forum_id: %{type: :integer},
+             user_id: %{type: :integer},
+             section_id: %{type: :integer},
+             title: %{type: :text, analyzer: :smartcn},
+             content: %{type: :text, analyzer: :smartcn},
+             is_top: %{type: :boolean},
+             is_hot: %{type: :boolean},
+             is_vote: %{type: :boolean},
+             reads: %{type: :integer},
+             comms: %{type: :integer},
+             created_at: %{type: :date},
+             last_reply_at: %{type: :date},
+             active: %{type: :boolean},
+             has_pic: %{type: :boolean}
+           }
+         }
+
+         Elasticsearch.create_index("forum", settings, %{
+           posts: posts_mapping
+         })
+    end
   end
-
-  unless Elasticsearch.is_index?("forum") do
-     settings = %{
-       number_of_shards: 5,
-       number_of_replicas: 1,
-     }
-
-     posts_mapping = %{
-       properties: %{
-         id: %{type: :integer},
-         forum_id: %{type: :integer},
-         user_id: %{type: :integer},
-         section_id: %{type: :integer},
-         title: %{type: :text, analyzer: :smartcn},
-         content: %{type: :text, analyzer: :smartcn},
-         is_top: %{type: :boolean},
-         is_hot: %{type: :boolean},
-         is_vote: %{type: :boolean},
-         reads: %{type: :integer},
-         comms: %{type: :integer},
-         created_at: %{type: :date},
-         last_reply_at: %{type: :date},
-         active: %{type: :boolean},
-         has_pic: %{type: :boolean}
-       }
-     }
-
-     Elasticsearch.create_index("forum", settings, %{
-       posts: posts_mapping
-     })
-   end
- end
 
 end

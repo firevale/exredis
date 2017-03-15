9392d6758f80ceb52f5a4966d109ebf40c43f803
Minzz <zhumingzhen@firevale.com>
2017-03-15 18:11:03 +0800

    wip

--- static/forum/store/getters.js
+++ static/forum/store/getters.js
@@ -5,8 +5,6 @@ const currentSectionId = state => state.forum.currentSectionId
 const postsOrderByField = state => state.forum.postsOrderByField
 const searchKeyword = state => state.search.searchKeyword
 const searchKeywordHistory = state => state.search.historyKeywords
-const searchPageCount = state => state.search.pageCount
-const searchCurrentPage = state => state.search.currentPage
 const userInfo = state => state.user
 const commonIssues = state => state.commonIssues.issues
 const forumInfo = state => state.forum.info
@@ -18,8 +16,6 @@ export {
   postsOrderByField,
   searchKeyword,
   searchKeywordHistory,
-  searchPageCount,
-  searchCurrentPage,
   userInfo,
   commonIssues,
   forumInfo,

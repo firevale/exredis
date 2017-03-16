b83b98c2f1275555ae7c453bb3e740127958c349
Minzz <zhumingzhen@firevale.com>
2017-03-16 18:56:00 +0800

    image upload

--- static/forum/views/personalPage.vue
+++ static/forum/views/personalPage.vue
@@ -1,198 +1,227 @@
 <template>
-<div>
-  <div class="row-line content-item">
-    <div class="column-line" style="margin-left: 1rem;">
-      <figure class="image is-64x64" style="margin: auto;border-radius:50%;border: 1px solid;overflow: hidden;">
-        <img :src="this.userInfo.avatar_url"></img>
-      </figure>
-    </div>
-    <div class="column-line" style="flex: 1;text-align: left;margin: 0 3rem;">
-      <div>
-        {{ $t('forum.personal.nickName') }}
-        <span>{{ this.userInfo.nickName }}</span>
+  <div>
+    <div class="row-line content-item">
+      <div class="column-line" style="margin-left: 1rem;">
+        <figure class="image is-64x64" style="margin: auto;border-radius:50%;border: 1px solid;overflow: hidden;" @click="onShowImageUpload">
+          <img :src="this.userInfo.avatar_url"></img>
+        </figure>
+        <img-upload url="https://httpbin.org/post" @crop-success="cropSuccess" @crop-upload-success="cropUploadSuccess" @crop-upload-fail="cropUploadFail" field="avatar" v-model="showImgUpload"></img-upload>
+      </div>
+      <div class="column-line" style="flex: 1;text-align: left;margin: 0 3rem;">
+        <div>
+          {{ $t('forum.personal.nickName') }}
+          <span>{{ this.userInfo.nickName }}</span>
+        </div>
+        <div>
+          {{ $t('forum.personal.postCount') }}
+          <span>{{ this.postRecords }}</span>
+        </div>
+        <div>
+          {{ $t('forum.personal.registerTime') }}
+          <span>{{ this.userInfo.reg_at | formatServerDateTime }}</span>
+        </div>
       </div>
       <div>
-        {{ $t('forum.personal.postCount') }}
-        <span>{{ this.postRecords }}</span>
-      </div>
-      <div>
-        {{ $t('forum.personal.registerTime') }}
-        <span>{{ this.userInfo.reg_at | formatServerDateTime }}</span>
       </div>
     </div>
-    <div>
+    <div class="horizontal-seperator"></div>
+    <div class="is-child  row-menu">
+      <div class="service-menu" :class="{'menu-selected': type=='myPosts'}" @click="switchMenu('myPosts')">
+        <span>{{ $t('forum.personal.myPosts') }}</span>
+      </div>
+      <div class="service-menu" :class="{'menu-selected': type=='myComments'}" @click="switchMenu('myComments')">
+        <span>{{ $t('forum.personal.myComments') }}</span>
+      </div>
+      <div class="service-menu" :class="{'menu-selected': type=='myFavor'}" @click="switchMenu('myFavor')">
+        <span>{{ $t('forum.personal.myFavor') }}</span>
+      </div>
+    </div>
+    <div class="horizontal-seperator">
+      <div :class="{'move-box-left': type=='myPosts','move-box-center': type=='myComments','move-box-right': type=='myFavor'}">
+        <div class="arrow-down"></div>
+      </div>
+    </div>
+    <div class="content-item" v-show="type == 'myPosts'">
+      <my-posts v-for="(item, index) in postList" :item-data="item" :on-item-deleted="onItemDelete" :item-index="index"></my-posts>
+    </div>
+    <div class="content-item" v-show="type == 'myComments'">
+      <my-comments v-for="item in commentList" :item-data="item"></my-comments>
+    </div>
+    <div class="content-item" v-show="type == 'myFavor'">
+      <my-favorate v-for="(item, index) in favoriteList" :item-data="item" :on-item-deleted="onItemDelete" :item-index="index"></my-favorate>
+    </div>
+    <div class="column is-full" v-show="total > 1">
+      <pagination ref="pag" :on-page-change="onPageChange" :page-count="total" :current-page="page"></pagination>
     </div>
   </div>
-  <div class="horizontal-seperator"></div>
-  <div class="is-child  row-menu">
-    <div class="service-menu" :class="{'menu-selected': type=='myPosts'}" @click="switchMenu('myPosts')">
-      <span>{{ $t('forum.personal.myPosts') }}</span>
-    </div>
-    <div class="service-menu" :class="{'menu-selected': type=='myComments'}" @click="switchMenu('myComments')">
-      <span>{{ $t('forum.personal.myComments') }}</span>
-    </div>
-    <div class="service-menu" :class="{'menu-selected': type=='myFavor'}" @click="switchMenu('myFavor')">
-      <span>{{ $t('forum.personal.myFavor') }}</span>
-    </div>
-  </div>
-  <div class="horizontal-seperator">
-    <div :class="{'move-box-left': type=='myPosts','move-box-center': type=='myComments','move-box-right': type=='myFavor'}">
-      <div class="arrow-down"></div>
-    </div>
-  </div>
-  <div class="content-item" v-show="type == 'myPosts'">
-    <my-posts v-for="(item, index) in postList" :item-data="item" :on-item-deleted="onItemDelete" :item-index="index"></my-posts>
-  </div>
-  <div class="content-item" v-show="type == 'myComments'">
-    <my-comments v-for="item in commentList" :item-data="item"></my-comments>
-  </div>
-  <div class="content-item" v-show="type == 'myFavor'">
-    <my-favorate v-for="(item, index) in favoriteList" :item-data="item" :on-item-deleted="onItemDelete"
-      :item-index="index"></my-favorate>
-  </div>
-  <div class="column is-full" v-show="total > 1">
-    <pagination ref="pag" :on-page-change="onPageChange" :page-count="total" :current-page="page"></pagination>
-  </div>
-</div>
 </template>
 <script>
-import {
-  mapGetters,
-  mapActions
-} from 'vuex'
-import myPosts from "../components/myPosts"
-import myFavorate from "../components/myFavorate"
-import myComments from "../components/myComments"
-import pagination from "../components/pagination"
+  import {
+    mapGetters,
+    mapActions
+  } from 'vuex'
 
-export default {
-  components: {
-    myPosts,
-    myFavorate,
-    pagination,
-    myComments,
-  },
+  import myPosts from "../components/myPosts"
+  import myFavorate from "../components/myFavorate"
+  import myComments from "../components/myComments"
+  import pagination from "../components/pagination"
+  import imgUpload from "vue-image-crop-upload"
 
-  mounted: function() {
-    this.$refs.pag.$on('switch-page', this.getPostPage)
-    this.getUserInfo()
-    this.getPostPage(this.page)
-  },
-
-  computed: {
-    ...mapGetters([
-      'userInfo'
-    ]),
-
-  },
-
-  data() {
-    return {
-      type: 'myPosts',
-      postList: [],
-      commentList: [],
-      favoriteList: [],
-      page: 1,
-      total: 1,
-      recordsPerPage: 6,
-      postRecords: 0,
-    }
-  },
-  methods: {
-    ...mapActions([
-      'serUserProfile'
-    ]),
-
-    switchMenu: function(menu) {
-      if (menu != this.type) {
-        this.page = 1
-        this.total = 1
-        this.type = menu
-        this.onPageChange(this.page)
-      }
+  export default {
+    components: {
+      myPosts,
+      myFavorate,
+      pagination,
+      myComments,
+      imgUpload,
     },
 
-    onPageChange: function(page) {
-      switch (this.type) {
-        case "myPosts":
-          this.getPostPage(page)
-          break;
-        case "myComments":
-          this.getCommentPage(page)
-          break;
-        case "myFavor":
-          this.getFavoritePage(page)
-          break;
-      }
+    mounted: function() {
+      this.$refs.pag.$on('switch-page', this.getPostPage)
+      this.getUserInfo()
+      this.getPostPage(this.page)
     },
 
-    onItemDelete(index) {
-      switch (this.type) {
-        case "myPosts":
-          this.postRecords--;
-          this.postList.splice(index, 1)
-          break;
-        case "myFavor":
-          this.favoriteList.splice(index, 1)
-          break;
-      }
+    computed: {
+      ...mapGetters([
+        'userInfo'
+      ]),
+
     },
 
-    getUserInfo: async function() {
-      let result = await this.$acs.getUserInfo()
-
-      if (result.success) {
-        this.serUserProfile(result.user)
+    data() {
+      return {
+        type: 'myPosts',
+        postList: [],
+        commentList: [],
+        favoriteList: [],
+        page: 1,
+        total: 1,
+        recordsPerPage: 6,
+        postRecords: 0,
+        showImgUpload: false
       }
     },
+    methods: {
+      ...mapActions([
+        'serUserProfile'
+      ]),
 
-    getPostPage: async function(page) {
-      if (!this.processing) {
-        this.processing = true
+      switchMenu: function(menu) {
+        if (menu != this.type) {
+          this.page = 1
+          this.total = 1
+          this.type = menu
+          this.onPageChange(this.page)
+        }
+      },
+      onShowImageUpload: function() {
+        console.info("image upload...")
+        this.showImgUpload = true
+      },
+      cropSuccess(data, field, key) {
+        alert('success')
+      },
+      cropUploadSuccess(data, field, key) {
+        console.log('-------- 上传成功 --------');
+        console.log(data);
+        console.log('field: ' + field);
+        console.log('key: ' + key);
+      },
+      cropUploadFail(status, field, key) {
+        console.log('-------- 上传失败 --------');
+        console.log(status);
+        console.log('field: ' + field);
+        console.log('key: ' + key);
+      },
 
-        let result = await this.$acs.getUserPagedPost(this.$router.currentRoute.params.forumId,
-          page, this.recordsPerPage)
+      onPageChange: function(page) {
+        s
+        switch (this.type) {
+          case "myPosts":
+            this.getPostPage(page)
+            break;
+          case "myComments":
+            this.getCommentPage(page)
+            break;
+          case "myFavor":
+            this.getFavoritePage(page)
+            break;
+        }
+      },
+
+      onItemDelete(index) {
+        switch (this.type) {
+          case "myPosts":
+            this.postRecords--;
+            this.postList.splice(index, 1)
+            break;
+          case "myFavor":
+            this.favoriteList.splice(index, 1)
+            break;
+        }
+      },
+
+      getUserInfo: async function() {
+        let result = await this.$acs.getUserInfo()
 
         if (result.success) {
-          this.postList = result.posts
-          this.total = result.total
-          this.page = page
-          this.postRecords = result.records
+          this.serUserProfile(result.user)
         }
-        this.processing = false
-      }
-    },
+      },
 
-    getCommentPage: async function(page) {
-      if (!this.processing) {
-        this.processing = true
+      getPostPage: async function(page) {
+        if (!this.processing) {
+          this.processing = true
 
-        let result = await this.$acs.getUserPostComments(page, this.recordsPerPage)
+          let result = await this.$acs.getUserPagedPost(this.$router.currentRoute.params.forumId,
+            page, this.recordsPerPage)
 
-        if (result.success) {
-          this.commentList = result.comments
-          this.total = result.total
-          this.page = page
+          if (result.success) {
+            this.postList = result.posts
+            this.total = result.total
+            this.page = page
+            this.postRecords = result.records
+          }
+          this.processing = false
         }
-        this.processing = false
-      }
-    },
+      },
 
-    getFavoritePage: async function(page) {
-      if (!this.processing) {
-        this.processing = true
+      getCommentPage: async function(page) {
+        if (!this.processing) {
+          this.processing = true
 
-        let result = await this.$acs.getUserPostFavorites(page, this.recordsPerPage)
+          let result = await this.$acs.getUserPostComments(page, this.recordsPerPage)
 
-        if (result.success) {
-          this.favoriteList = result.favorites
-          this.total = result.total
-          this.page = page
+          if (result.success) {
+            this.commentList = result.comments
+            this.total = result.total
+            this.page = page
+          }
+          this.processing = false
         }
-        this.processing = false
-      }
-    },
+      },
 
-  },
-}
+      getFavoritePage: async function(page) {
+        if (!this.processing) {
+          this.processing = true
+
+          let result = await this.$acs.getUserPostFavorites(page, this.recordsPerPage)
+
+          if (result.success) {
+            this.favoriteList = result.favorites
+            this.total = result.total
+            this.page = page
+          }
+          this.processing = false
+        }
+      },
+
+    },
+  }
 </script>
+<style lang="css">
+  .vicp-drop-area {
+    height: 175px;
+  }
+</style>

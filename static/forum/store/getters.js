28a5dd3751819c8ada205eb99515d9d674148b0e
Minzz <zhumingzhen@firevale.com>
2017-03-16 10:44:31 +0800

    wip

--- static/forum/store/getters.js
+++ static/forum/store/getters.js
@@ -4,15 +4,7 @@ const transitionName = state => state.app.transitionName
 const currentSectionId = state => state.forum.currentSectionId
 const postsOrderByField = state => state.forum.postsOrderByField
 const searchKeyword = state => state.search.searchKeyword
-const searchKeywordHistory = state => {
-  if (localStorage.historyKeywords) {
-    return JSON.parse(localStorage.historyKeywords)
-  } else {
-    return []
-  }
-
-}
-
+const searchKeywordHistory = state => state.search.historyKeywords
 const userInfo = state => state.user
 const commonIssues = state => state.commonIssues.issues
 const forumInfo = state => state.forum.info
--- static/forum/store/modules/search.js
+++ static/forum/store/modules/search.js
@@ -1,46 +1,46 @@
 import * as types from '../mutationTypes'
 
+const storage_search_history = '_acs_forum_search_historys_'
+
+function restoreSearchHistorys() {
+  let storageData = localStorage.getItem(storage_search_history)
+  if (storageData) {
+    return JSON.parse(storageData)
+  } else {
+    return []
+  }
+}
+
 const state = {
   searchKeyword: '',
-  historyKeywords: [
-    '攻略',
-    '任务',
-    '赚钱',
-    '综合讨论',
-    '副本攻略',
-    '指南',
-  ],
+  historyKeywords: restoreSearchHistorys(),
 }
 
 const mutations = {
   [types.SET_SEARCH_KEYWORD](state, keyword) {
     state.searchKeyword = keyword
     if (keyword) {
-      let historyKeywords = []
-      if (localStorage.historyKeywords) {
-        historyKeywords = JSON.parse(localStorage.historyKeywords)
+      if (state.historyKeywords[0] && state.historyKeywords[0] != keyword) {
+        state.historyKeywords.unshift(keyword)
+      } else if (state.historyKeywords.length == 0) {
+        state.historyKeywords.unshift(keyword)
       }
 
-      if (historyKeywords[0] && historyKeywords[0] != keyword) {
-        historyKeywords.unshift(keyword)
-      } else if (historyKeywords.length == 0) {
-        historyKeywords.unshift(keyword)
+      if (state.historyKeywords.length > 6) {
+        state.historyKeywords.pop()
       }
 
-      if (historyKeywords.length > 6) {
-        historyKeywords.pop()
-      }
-
-      localStorage.historyKeywords = JSON.stringify(historyKeywords)
+      localStorage.setItem(storage_search_history, JSON.stringify(state.historyKeywords))
     }
   },
 
   [types.CLEAR_SEARCH_HISTORY](state) {
-    localStorage.removeItem("historyKeywords")
-  }
+    state.historyKeywords = []
+    localStorage.removeItem(storage_search_history)
+  },
 }
 
 export default {
   state,
-  mutations,
+  mutations
 }

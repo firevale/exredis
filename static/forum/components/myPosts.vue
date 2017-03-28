6558c308322d19bebc962a5767a7a9304d27343d
Minzz <zhumingzhen@firevale.com>
2017-03-28 17:46:25 +0800

    wip

--- static/forum/components/myPosts.vue
+++ static/forum/components/myPosts.vue
@@ -1,72 +1,73 @@
 <template>
-  <div class="my-posts-list-item row-line">
-    <div style="flex: 1;text-align: left;">
-      <div @click="showDetail">
-        <span class="post-title">[{{ itemData.section.title }}] {{ itemData.title | filterKeyword}}</span>
-        <a v-show="itemData.newComment" class="button excellent-btn">{{ $t('forum.personal.newComment') }}</a>
+  <article class="content-item">
+      <div  style="display: flex; flex-direction: row; align-items: center; justify-content: flex-start;margin-bottom:.8em;" @click="showDetail">
+        <h5 class="title is-5">[{{ itemData.section.title }}] {{ itemData.title | filterKeyword}}</h5>
+        <a v-show="!itemData.newComment" class="tag is-outlined">{{ $t('forum.personal.newComment') }}</a>
       </div>
-      <div class="post-info">
-        <span>{{ itemData.inserted_at | formatServerDateTime }}</span>
-        <span> | </span>
-        <span>{{ itemData.comms + '/' + itemData.reads }}</span>
+      <div class="level">
+         <div class="level-left level-item">
+           <span class="subtitle">{{ itemData.inserted_at | formatServerDateTime }}</span>
+           <span class="subtitle" style="margin:0 1em">|</span>
+           <span class="subtitle">{{ itemData.comms + '/' + itemData.reads }}</span>
+         </div>
+         <div class="level-right level-item">
+            <span class="icon image-icon icon-trash is-small" @click.prevent="confirmDeletePost"></span>
+            <span class="is-danger" @click.prevent="confirmDeletePost"> {{ $t('forum.personal.deleteBtn') }}</span>
+         </div>
       </div>
-    </div>
-    <div>
-      <span class="button" @click.prevent="confirmDeletePost">{{ $t('forum.personal.deleteBtn') }}</span>
-    </div>
-  </div>
+  </article>
 </template>
 <script>
 import AlertDialog from './alertDialog'
 import message from './message'
 
-  export default {
-    props: {
-      itemData: {
-        type: Object,
-        default: null,
-      },
-      itemIndex: {
-        type: Number,
-      },
-      onItemDeleted: {
-        type: Function,
-        default: undefined
-      }
+export default {
+  props: {
+    itemData: {
+      type: Object,
+      default: null,
+    },
+    itemIndex: {
+      type: Number,
+    },
+    onItemDeleted: {
+      type: Function,
+      default: undefined
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    showDetail() {
+      this.$router.push({
+        name: 'detail',
+        params: {
+          postId: this.itemData.id
+        },
+      })
     },
 
-    computed: {
-
+    confirmDeletePost() {
+      AlertDialog.showModal({
+        message: this.$t('forum.detail.deletePostTip'),
+        onOk: async _ => {
+          let result = await this.$acs.togglePostStatus({
+            post_id: this.itemData.id,
+            active: false
+          })
+          if (result.success) {
+            message.showMsg(this.$t(result.i18n_message))
+            if (this.onItemDeleted)
+              this.onItemDeleted(this.itemIndex)
+          }
+        },
+        onCancel: null,
+      })
     },
 
-    methods: {
-      showDetail() {
-        this.$router.push({
-          name: 'detail',
-          params: {
-            postId: this.itemData.id
-          },
-        })
-      },
-
-      confirmDeletePost() {
-        AlertDialog.showModal({
-          message: this.$t('forum.detail.deletePostTip'),
-          onOk: async _ => {
-            let result = await this.$acs.togglePostStatus({
-              post_id: this.itemData.id,
-              active: false
-            })
-            if (result.success) {
-              message.showMsg(this.$t(result.i18n_message))
-              if (this.onItemDeleted)
-                this.onItemDeleted(this.itemIndex)
-            }
-          },
-          onCancel: null,
-        })
-      },
-
-    },
-  }
+  },
+}
 </script>

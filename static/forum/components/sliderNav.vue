5985d49aba40169adca0d8e28c1b554749ead588
Minzz <zhumingzhen@firevale.com>
2017-04-01 13:41:17 +0800

    wip

--- static/forum/components/sliderNav.vue
+++ static/forum/components/sliderNav.vue
@@ -55,13 +55,13 @@ export default {
       position: relative;
       .slider-bar {
         position: absolute;
-        bottom: -.7em;
+        bottom: -.8em;
         content: "";
         width: 100%;
         height: 1em;
         background-image: url("~forum/assets/nav-narrow@3x.png");
         background-repeat: no-repeat;
-        background-size: 9em 0.7em;
+        background-size: 9em 0.8em;
         transition: background-position 0.3s;
         transition-timing-function: ease;
       }

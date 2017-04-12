7dcbec28552a2b2bec81e6c189d4f70deacbe714
xiaobin <xiaobin@firevale.com>
2017-04-12 15:54:59 +0800

    wip

--- static/app/views/forum/personalPage.vue
+++ static/app/views/forum/personalPage.vue
@@ -3,7 +3,7 @@
     <article class="media info">
       <figure class="media-left" @click="onShowImageUpload">
         <p class="image is-64x64 avatar-image">
-          <img :src="this.userInfo.avatar_url"></img>
+          <img :src="avatarUrl"></img>
         </p>
         <img-upload url="/forum_actions/update_user_avatar" @crop-upload-success="cropUploadSuccess" @crop-upload-fail="cropUploadFail"
           field="avatar" :params="uploadParams" v-model="showImgUpload"></img-upload>
@@ -88,6 +88,9 @@ export default {
         case "myFavor":
           return "90% bottom"
       }
+    },
+    avatarUrl() {
+      this.userInfo.avatar_url ? this.userInfo.avatar_url : window.acsConfig.defaultAvatarUrl
     }
   },
 

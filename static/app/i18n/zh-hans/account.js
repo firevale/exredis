155a638e5b7de0d3db9d0f39713890d3ae9490e7
xiaobin <xiaobin@firevale.com>
2017-04-14 16:10:20 +0800

    wip

--- static/app/i18n/zh-hans/account.js
+++ static/app/i18n/zh-hans/account.js
@@ -1,3 +1,11 @@
 export default {
   accountTitle: '个人中心',      
+  userId: '用户ID',
+  nickname: '昵称',
+  mobile: '绑定手机',
+  email: '绑定邮箱',
+  residentInfo: '实名认证',
+  logout: '退出登录',
+  notBound: '未绑定',
+  notAuthenticated: '未认证',
 }

88387484772b86cb5a13799436d105da5e01f8b1
Minzz <zhumingzhen@firevale.com>
2017-04-14 20:35:36 +0800

    wip

---
+++ static/admin/views/malls/orderInfo.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="box tile is-ancestor">
+    <div class="tile is-parent is-vertical">
+      <div class="tile is-child">
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.id')}}:{{orderInfo.id}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.status')}}:{{orderInfo.id}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.inserted_at')}}:{{orderInfo.id}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.email')}}:{{orderInfo.id}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.inserted_at')}}:{{orderInfo.id}}</h6>
+      </div>
+      <div class="tile is-child">
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.user_address.name')}}:{{orderInfo.user_address.name}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.user_address.mobile')}}:{{orderInfo.user_address.mobile}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.user_address.address')}}:{{orderInfo.user_address.area}}{{orderInfo.user_address.address}}</h6>
+      </div>
+      <div class="title is-child">
+        <div class="box columns" style="border-radius: 0;margin:0;padding:.5rem;">
+          <div v-for="detail in orderInfo.details" class="cloumn">
+            <div class="media" style="margin-right:1rem;">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img :src="detail.goods_pic">
+                </p>
+              </figure>
+              <div class="media-content">
+                <h6 class="subtitle is-6">{{detail.goods_name}}经典红色款</h6>
+                <h6 class="subtitle is-6"> {{detail.price}}</h6>
+                <h6 class="subtitle is-6"> X{{detail.amount}} </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tile is-child">
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.postage')}}:{{orderInfo.user_address.name}}</h6>
+        <h6 class="subtitle is-6">{{$t('admin.mall.order.fields.total')}}:{{orderInfo.user_address.mobile}}</h6>
+        <!--<span class="subtitle is-6">{{$t('admin.mall.order.fields.paid_type')}}:{{orderInfo.paid_type}}</span>
+        <span class="subtitle is-6">{{$t('admin.mall.order.fields.transaction_id')}}:{{orderInfo.transaction_id}}</span>-->
+      </div>
+    </div>
+    <div class="tile">
+      <h6 class="subtitle is-6">历史记录:</h6>
+    </div>
+  </div>
+</template>
+<script>
+import {
+  required,
+  minLength,
+  maxLength
+} from 'vuelidate/lib/validators'
+
+import {
+  openNotification,
+  processAjaxError
+} from 'admin/miscellaneous'
+
+import imgUpload from "vue-image-crop-upload/upload-2.vue"
+
+import {
+  showFileUploadDialog
+} from 'common/components/fileUpload'
+
+const touchMap = new WeakMap()
+
+export default {
+  components: {
+    imgUpload,
+  },
+
+  mounted: function() {
+    this.orderInfo = this.$route.params.orderInfo
+  },
+
+  data() {
+    return {
+      orderInfo: undefined,
+    }
+  },
+  watch: {
+
+  },
+
+  methods: {
+    getPrice: function(price) {
+      if (price)
+        return parseFloat(price / 100).toFixed(2)
+      else
+        return 0
+    },
+
+    setEditor: function(editor) {
+      this.editor = editor
+    },
+
+    handleValidation: function($v) {
+      $v.$reset()
+      if (touchMap.has($v)) {
+        clearTimeout(touchMap.get($v))
+      }
+      touchMap.set($v, setTimeout($v.$touch(), 2000))
+    },
+
+    onShowImageUpload: function() {
+      if (this.goods.id.length > 0) {
+        this.showImgUpload = true
+      } else {
+        openNotification({
+          title: this.$t('admin.titles.warning'),
+          message: this.$t('admin.mall.goods.saveFirst'),
+          type: 'danger',
+          duration: 4500,
+          container: '.notifications',
+        })
+      }
+    },
+
+    onInsertImage: function(editor) {
+      showFileUploadDialog({
+        postAction: '/admin_actions/update_goods_pic',
+        accept: 'image/jpeg, image/png',
+        data: {
+          app_id: this.goods.app_id
+        },
+        extensions: ['png', 'jpg', 'jpeg'],
+        callback: response => {
+          if (response.success) {
+            editor.focus()
+            let range = editor.getSelection()
+            editor.insertEmbed(range.index, 'image', response.link)
+          } else if (response.i18n_message) {
+            message.showMsg(this.$t(response.i18n_message, response.i18n_message_object))
+          } else if (response.message) {
+            message.showMsg(response.message)
+          } else {
+            message.showMsg(this.$t('admin.error.networkError'))
+          }
+        },
+      })
+    },
+
+    onDelete: function() {
+      // 点击删除按钮，需判断该商品是否已有销量，销量不为0的商品不可删除，需文字提示：该商品已销售不可删除；
+      // 商品销量为0，则可删除，仍需有二次文字信息提示：请确认是否删除该商品？
+      // 选择确认，则成功在前后端删除，返回商品管理页；
+      // 选择取消，则不删除，返回商品详情页；
+    },
+
+    onPublish: function() {
+      // 点击发布按钮，判断必填项是否为空或都按规则填写； // 全部正确填写，则成功发布，发布按钮变为取消按钮，可在前端显示并搜索到该商品； // 为空或未按规则填写，则需分别报错提示： // 请上传正确的商品图片；
+      // 请正确填写商品名称/单价／邮费/库存/商品详情； // 点击取消按钮，取消按钮变为发布按钮，不可在前端显示并搜索到该商品；
+    },
+
+    onSave: function() {
+      if (this.goods.name.length == 0 || this.goods.description.length == 0 || this.goods.price.length ==
+        0 ||
+        this.goods.stock.length == 0 || this.goods.postage.length == 0 || this.goods.id.length ==
+        0) {
+        openNotification({
+          title: this.$t('admin.titles.warning'),
+          message: this.$t('admin.mall.goods.pleaseFill'),
+          type: 'danger',
+          duration: 4500,
+          container: '.notifications',
+        })
+        return;
+      }
+
+      this.handleSubmit()
+    },
+
+    handleSubmit: async function() {
+      this.saving = true
+      let result = await this.$acs.updateGoods({
+        id: this.goods.id,
+        app_id: this.goods.app_id,
+        name: this.goods.name,
+        pic: this.goods.pic,
+        description: this.goods.description,
+        price: this.goods.price,
+        postage: this.goods.postage,
+        stock: this.goods.stock,
+        currency: this.goods.currency,
+        is_new: this.isNew
+      })
+      if (result.success) {
+        this.$router.go(-1)
+      }
+      this.saving = false
+    },
+  }
+}
+</script>
+<style lang="scss">
+.tile .subtitle:not(:last-child) {
+  margin-bottom: .8rem;
+}
+</style>

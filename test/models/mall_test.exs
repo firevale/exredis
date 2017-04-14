dc80dcc2873c787a133a3c849adcff449d06b33b
xiaobin <xiaobin@firevale.com>
2017-04-14 10:24:35 +0800

    rename app_mall test groupds

--- test/models/mall_test.exs
+++ test/models/mall_test.exs
@@ -1,18 +1,18 @@
-defmodule Acs.AppMallGoodsTest do
+defmodule Acs.MallTest do
   use Acs.ModelCase
 
-  alias Acs.AppMallGoods
+  alias Acs.Mall
 
   @valid_attrs %{title: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
-    changeset = AppMallGoods.changeset(%AppMallGoods{}, @valid_attrs)
+    changeset = Mall.changeset(%Mall{}, @valid_attrs)
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
-    changeset = AppMallGoods.changeset(%AppMallGoods{}, @invalid_attrs)
+    changeset = Mall.changeset(%Mall{}, @invalid_attrs)
     refute changeset.valid?
   end
 end

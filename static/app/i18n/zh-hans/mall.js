e1681e5b4347d99562d977d1b53df3b0d61fdd3f
zhongxb <zhongxb@firevale.com>
2017-04-17 15:44:47 +0800

    wip

--- static/app/i18n/zh-hans/mall.js
+++ static/app/i18n/zh-hans/mall.js
@@ -6,5 +6,9 @@ export default {
         stock: '库存',
         buyNow: '立即购买',
         soldOut: '已售罄'
+    },
+    order: {
+        addressPlaceholder: '请填写收货地址',
+        totalPrice: '合计：{currency} {price}（含邮费 {currency} {postage}）',
     }
 }

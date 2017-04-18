61a7990c80dc90cf8310d57b3f185d3b8e722516
Minzz <zhumingzhen@firevale.com>
2017-04-18 15:40:54 +0800

    wip

--- static/app/i18n/zh-hans/mall.js
+++ static/app/i18n/zh-hans/mall.js
@@ -12,9 +12,44 @@ export default {
     totalPrice: '合计：{price}（含邮费 {postage}）',
     wechatPay: '微信支付',
     aliPay: '支付宝',
+    fields: {
+      id: '订单号',
+      postage: '邮费',
+      total: '合计',
+      status: '订单状态',
+      inserted_at: '下单时间',
+      email: '用户帐号',
+      paid_type: {
+        label: '支付渠道',
+        wechat: '微信'
+      },
+      transaction_id: '支付单号',
+      user_address: {
+        name: '姓名',
+        mobile: '电话',
+        address: '地址',
+      },
+    },
+    status: {
+      '-3': '已退款',
+      '-2': '已退货',
+      '-1': '已关闭',
+      '0': '待支付',
+      '1': '已支付',
+      '2': '待收货',
+      '3': '待确认',
+      '4': '已完成'
+
+    }
   },
   mine: {
-
+    myOrder:{
+      tabs:{
+        all: '全部',
+        unPay:'待付款',
+        unReceived:'未收货'
+      }
+    }
   },
   titles: {
     goodsDetail: '商品详情',
---
+++ static/app/views/mall/myOrders.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <slider-nav :menus="menus" :selectedValue="type" @onSelect="switchMenu" ref="nav"></slider-nav>
+    <div>
+      <order-item v-for="order in orders" :order="order"></order-item>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from '../../vue-installed'
+import {
+  mapGetters,
+  mapActions
+} from 'vuex'
+
+import * as acs from 'common/js/acs'
+import nativeApi from 'common/js/nativeApi'
+import * as filter from 'common/js/filters'
+
+import sliderNav from '../../components/sliderNav'
+import scroller from 'common/components/scroller'
+
+import orderItem from '../../components/orderItem'
+export default {
+  components: {
+    sliderNav,
+    scroller,
+    orderItem
+  },
+  mounted: async function() {
+    let result = await this.$acs.fetchMyOrders('all', this.page + 1, this.records_per_page)
+    if (result.success) {
+      this.orders = result.orders
+
+      this.total = result.total
+      this.page = this.page + 1
+    }
+  },
+  data: function() {
+    return {
+      menus: [{
+          text: this.$t('mall.mine.myOrder.tabs.all'),
+          value: 'all'
+        }, {
+          text: this.$t('mall.mine.myOrder.tabs.unPay'),
+          value: 'unPay'
+        },
+        {
+          text: this.$t('mall.mine.myOrder.tabs.unReceived'),
+          value: 'unReceived'
+        }
+      ],
+      type: 'all',
+      page: 0,
+      total: 1,
+      records_per_page: 10,
+      orders: []
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'transitionName',
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'setTransitionName'
+    ]),
+    switchMenu: function(item, index) {
+      this.$router.push({
+        name: item.value
+      })
+    }
+  }
+}
+</script>

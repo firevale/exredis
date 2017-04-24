573d0d901a5901fc2e9da523c6283c9b5a5dda11
xiaobin <xiaobin@firevale.com>
2017-04-24 18:59:42 +0800

    wip

--- static/app/views/forum/newPost.vue
+++ static/app/views/forum/newPost.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    <div class="has-text-left" style="padding: 1rem 0;">
-      <span class="title is-5 is-clickable" style="margin: 0 0.5rem 0 0; font-weight: 400" @click="showSelectSectionMenu">
+  <div style="position: relative">
+    <scroller>
+      <div class="has-text-left" style="padding: 1rem 0;">
+        <v-touch tag="span" class="title is-5 is-clickable" style="margin: 0 0.5rem 0 0; font-weight: 400" @tap="showSelectSectionMenu">
          {{ selectedSectionTitle }}
-      </span>
-      <span class="icon image-icon icon-pull-down is-clickable" @click="showSelectSectionMenu"> 
-      </span>
-    </div>
-    <form class="post" @submit.prevent="handleSubmit" v-show="selectedSectionTitle">
-      <p class="control is-horizontal" style="margin-bottom: 1.5rem">
-        <input class="input" style="border-radius: 0" type="text" v-model.trim="editingPostData.title" :placeholder="$t('forum.newPost.titlePlaceholder')"></input>
-      </p>
-      <quill-editor v-model.trim="editingPostData.content" :placeholder="$t('forum.newPost.textAreaPlaceHolder')" @ready="setEditor"
-        @input="handleValidation($v.editingPostData.content)" @image="onInsertImage">
-      </quill-editor>
-      <div class="tile is-full has-text-left" style="margin-top: 0.5rem" v-show="errorHint">
-        <span class="icon is-sign">!</span>
-        <span class="is-primary" style="font-size: 1rem">{{errorHint}}</span>
+        </v-touch>
+        <v-touch tag="span" class="icon image-icon icon-pull-down is-clickable" @tap="showSelectSectionMenu"> 
+        </v-touch>
       </div>
-      <div class="tile is-full has-text-centered">
-        <p style="margin: 0 auto">
-          <input type="button" style="min-width: 8rem; padding-bottom: 0.4em; padding-top: 0.35em; margin: 0.5rem 0; display: inline-block"
-            @click="preview" :value="$t('forum.newPost.preview')" class="button is-info" :class="processing || $v.$invalid ? 'is-disabled' : ''"
-          />
-          <input type="submit" style="display: inline-block; font-size: 1rem;" :value="$t('forum.newPost.btnTitle')" class="button is-primary"
-            :class="processing || $v.$invalid ? 'is-disabled' : ''" />
+      <form class="post" @submit.prevent="handleSubmit" v-show="selectedSectionTitle">
+        <p class="control is-horizontal" style="margin-bottom: 1.5rem">
+          <input class="input" style="border-radius: 0" type="text" v-model.trim="editingPostData.title" :placeholder="$t('forum.newPost.titlePlaceholder')"></input>
         </p>
-      </div>
-    </form>
+        <quill-editor v-model.trim="editingPostData.content" :placeholder="$t('forum.newPost.textAreaPlaceHolder')" @ready="setEditor"
+          @input="handleValidation($v.editingPostData.content)" @image="onInsertImage">
+        </quill-editor>
+        <div class="tile is-full has-text-left" style="margin-top: 0.5rem" v-show="errorHint">
+          <span class="icon is-sign">!</span>
+          <span class="is-primary" style="font-size: 1rem">{{errorHint}}</span>
+        </div>
+        <div class="tile is-full has-text-centered">
+          <p style="margin: 0 auto">
+            <input type="button" style="min-width: 8rem; padding-bottom: 0.4em; padding-top: 0.35em; margin: 0.5rem 0; display: inline-block"
+              @click="preview" :value="$t('forum.newPost.preview')" class="button is-info" :class="processing || $v.$invalid ? 'is-disabled' : ''"
+            />
+            <input type="submit" style="display: inline-block; font-size: 1rem;" :value="$t('forum.newPost.btnTitle')" class="button is-primary"
+              :class="processing || $v.$invalid ? 'is-disabled' : ''" />
+          </p>
+        </div>
+      </form>
+    </scroller>
   </div>
 </template>
 <script>
@@ -45,6 +47,7 @@ import {
   showFileUploadDialog
 } from 'common/components/fileUpload'
 
+import scroller from 'common/components/scroller'
 import Toast from 'common/components/toast'
 
 import * as utils from 'common/js/utils'
@@ -195,6 +198,10 @@ export default {
         this.processing = false
       }
     },
+  },
+
+  components: {
+    scroller
   }
 }
 </script>

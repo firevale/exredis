2fcd502bf41d3828c6f714a205b50bc27274a901
xiebing <xiebing@firevale.com>
2017-04-24 15:54:57 +0800

    wip

--- static/common/components/citySelect/citySelect.vue
+++ static/common/components/citySelect/citySelect.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="field has-addons is-marginless is-paddingless">
-    <p class="control">
+  <div class="columns is-multiline is-mobile">
+    <div class="column is-12-mobile is-paddingless" style="margin-top:1rem;">
       <span class="select">
-        <select v-model="province">
+        <select v-model="province" class="select-content">
           <option value="">选择省</option>
           <option v-for="province in provinces" :value="province.code">{{province.name}}</option>
         </select>
       </span>
-    </p>
-    <p class="control">
+    </div>
+    <div class="column is-12 is-paddingless" style="margin-top:0.5rem;">
       <span class="select">
-        <select v-model="city">
+        <select v-model="city" class="select-content">
           <option value="">选择城市</option>
           <option v-for="city in cities" :value="city.code">{{city.name}}</option>
         </select>
       </span>
-    </p>
-    <p class="control">
+    </div>
+    <div class="column is-12 is-paddingless" style="margin-top:0.5rem;margin-bottom: 1rem;">
       <span class="select">
-        <select v-model="district">
+        <select v-model="district" class="select-content">
           <option value="">选择县/区</option>
           <option v-for="district in districts" :value="district.code">{{district.name}}</option>
         </select>
       </span>
-    </p>
-    <slot>
-    </slot>
+    </div>
   </div>
 </template>
 <script>
@@ -33,6 +31,27 @@ import citydata from './citydata.json';
 export default {
   mounted: function() {
     this.provinces = citydata;
+    this.oldProvince = this.province;
+    this.oldCity = this.city;
+    this.oldDistrict = this.district;
+
+    if (this.oldProvince != "") {
+      let seletedProvince = this.provinces.filter(function(item) {
+        return item.code == this.province;
+      }.bind(this));
+
+      if (seletedProvince.length) {
+        this.oldProvince = seletedProvince[0];
+        this.cities = seletedProvince[0]['children'];
+      }
+      let seletedCity = this.cities.filter(function(item) {
+        return item["code"] == this.city;
+      }.bind(this));
+      if (seletedCity.length) {
+        this.oldCity = seletedCity[0];
+        this.districts = seletedCity[0]['children'];
+      }
+    }
   },
   props: {
     _province: {
@@ -106,9 +125,14 @@ export default {
       delete this.oldProvince.children;
       delete this.oldCity.children;
       delete this.oldDistrict.children;
-      this.$emit('onSelect',this.oldProvince , this.oldCity, this.oldDistrict)
+      this.$emit('onSelect', this.oldProvince, this.oldCity, this.oldDistrict)
     }
   },
   computed: {}
 }
 </script>
+<style lang="scss">
+.select-content {
+  width: 50vw;
+}
+</style>

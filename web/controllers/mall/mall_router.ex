10c2427c292101b2c815bda0b28252fd7a014214
zhongxb <zhongxb@firevale.com>
2017-04-25 17:40:29 +0800

    wip

--- web/controllers/mall/mall_router.ex
+++ web/controllers/mall/mall_router.ex
@@ -48,25 +48,6 @@ defmodule Acs.MallRouter do
     post "/fetch_my_orders", MallOrderController, :fetch_my_orders
 
     post "/get_goods_stock", MallController, :get_goods_stock
-    # get   "/get_paged_post", ForumController, :get_paged_post
-    # post  "/get_paged_post", ForumController, :get_paged_postb
-    # post  "/get_user_paged_post", ForumController, :get_user_paged_post
-    # post  "/update_user_avatar", ForumController, :update_user_avatar
-
-    # post  "/add_post", ForumController, :add_post
-
-    # post  "/get_post_detail", ForumController, :get_post_detail
-    # post  "/get_post_comments", ForumController, :get_post_comments
-    # post  "/get_user_post_comments", ForumController, :get_user_post_comments
-
-    # post  "/delete_comment", ForumController, :delete_comment
-    # post  "/add_comment", ForumController, :add_comment
-    # post  "/toggle_post_favorite", ForumController, :toggle_post_favorite
-    # post  "/toggle_post_status", ForumController, :toggle_post_status
-    # post  "/get_user_favorites", ForumController, :get_user_favorites
-    # post  "/search", ForumController, :search
-
-    # post  "/upload_post_image", ForumController, :upload_post_image
   end
 
 end

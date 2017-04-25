fc0def781a4491f9a226c0ffa4e770f890740aba
xiaobin <xiaobin@firevale.com>
2017-04-25 13:51:57 +0800

    add i18n language

---
+++ static/common/i18n/en/common.js
@@ -0,0 +1,13 @@
+ export default {
+   ok: 'OK',
+   cancel: 'Cancel',
+   add: 'Add',
+   delete: 'Delete',
+   close: 'Close',
+   save: 'Save',
+   update: 'Update',
+   bind: 'Bind',
+   camera: 'Take pictures',
+   photoLib: 'Select from photo library',
+   upload: 'Upload',
+ }
---
+++ static/common/i18n/zh-hant/upload.js
@@ -0,0 +1,13 @@
+export default {
+  filename: '文件名',
+  filesize: '文件大小',
+  progress: '上傳進度',
+  speed: '上傳速率',
+  title: '上傳',
+  hint: '點擊, 或拖動圖片至此處',
+  fileIsTooLarge: '文件太大了，圖片文件不能超過{maxFileSize}',
+  invalidFileType: '錯誤的文件類型: {fileType}',
+  imgShouldBeSquare: '圖片必須為正方形',
+  imgWidthShouldGreaterThan: '圖片的寬度不得小於{minWidth}',
+  uploadImage: '上傳圖片',
+}

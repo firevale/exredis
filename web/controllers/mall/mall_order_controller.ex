ddca244dd6a73e67363ad084aa37bf6eeccd0812
zhongxb <zhongxb@firevale.com>
2017-04-25 12:04:01 +0800

    wip

--- web/controllers/mall/mall_order_controller.ex
+++ web/controllers/mall/mall_order_controller.ex
@@ -144,10 +144,11 @@ defmodule Acs.MallOrderController do
               MallGoods.changeset(goods, %{stock: goods.stock - quantity}) |> Repo.update()
 
               # add order
+              {:ok, gd} = Poison.encode(goods)
               order = %{"id": order_id, "platform": platform, "device_id": device_id, "user_ip": ip_address, 
                       "goods_name": goods.name, "price": goods.price, "postage": goods.postage, 
                       "discount": 0, "final_price": final_price, "currency": goods.currency, "paid_type": pay_type,
-                      "app_id": goods.app_id, "user_id": user_id, "address": address}
+                      "app_id": goods.app_id, "user_id": user_id, "address": address, "snapshots": gd}
 
               {:ok, mall_order} = MallOrder.changeset(%MallOrder{}, order) |> Repo.insert
 

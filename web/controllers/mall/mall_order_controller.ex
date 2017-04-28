807d405c9fdaff61860bdf3d0b3ec628d1eb38c1
Minzz <zhumingzhen@firevale.com>
2017-04-28 14:42:57 +0800

    wip

--- web/controllers/mall/mall_order_controller.ex
+++ web/controllers/mall/mall_order_controller.ex
@@ -29,23 +29,25 @@ defmodule Acs.MallOrderController do
       conn |> json(%{success: true, total: 0, orders: []})
     else
       total_page = round(Float.ceil(total / records_per_page))
-      query = from order in MallOrder,
-              left_join: details in assoc(order, :details),
-              left_join: user in assoc(order, :user),
-              select: map(order, [:id, :goods_name, :status, :price, :final_price, :currency, :postage, :inserted_at,
-                user: [:id, :nickname, :mobile],
-                details: [:id, :goods_name, :goods_pic, :price, :amount] ]),
-              where: order.app_id == ^app_id,
-              order_by: [desc: order.inserted_at],
-              limit: ^records_per_page,
-              offset: ^((page - 1) * records_per_page),
-              preload: [user: user, details: details]
+      query = 
+        from order in MallOrder,
+          left_join: details in assoc(order, :details),
+          left_join: user in assoc(order, :user),
+          select: map(order, [:id, :goods_name, :status, :price, :final_price, :currency, :postage, :inserted_at,
+            user: [:id, :nickname, :mobile],
+            details: [:id, :goods_name, :goods_pic, :price, :amount] ]),
+          where: order.app_id == ^app_id,
+          order_by: [desc: order.inserted_at],
+          limit: ^records_per_page,
+          offset: ^((page - 1) * records_per_page),
+          preload: [user: user, details: details]
 
-       query = if(String.length(keyword)>0) do
-                  query |> where([o], o.id in ^ids)
-               else
-                  query
-               end
+       query =
+         if(String.length(keyword)>0) do
+            query |> where([o], o.id in ^ids)
+          else
+            query
+          end
 
       orders = Repo.all(query)
       conn |> json(%{success: true, orders: orders, total: total_page})
@@ -77,12 +79,13 @@ defmodule Acs.MallOrderController do
         size: records_per_page,
       }
 
-      query = case Integer.parse(keyword) do
-              {int_keyword,""} ->
-                  update_in(query.query.bool.should,&(&1++[ %{term: %{user_id: int_keyword}}]))
-               _ ->
-                  query
-              end
+      query = 
+        case Integer.parse(keyword) do
+          {int_keyword,""} ->
+              update_in(query.query.bool.should,&(&1++[ %{term: %{user_id: int_keyword}}]))
+            _ ->
+              query
+        end
 
       case Elasticsearch.search(%{index: "mall", type: "orders", query: query, params: %{timeout: "1m"}}) do
         {:ok, %{hits: %{hits: hits, total: total}}} ->
@@ -103,16 +106,17 @@ defmodule Acs.MallOrderController do
   end
 
   defp fetch_order_info( order_id) do
-    query = from o in MallOrder,
-              left_join: details in assoc(o, :details),
-              left_join: user in assoc(o, :user),
-              left_join: op_logs in assoc(o, :op_logs),
-              select: map(o, [:id, :goods_name, :status, :price, :final_price, :address, :currency, :paid_type, :transaction_id, :postage, :inserted_at, :snapshots,
-                user: [:id, :nickname, :mobile, :email],
-                details: [:id, :goods_name, :goods_pic, :price, :amount, :mall_goods_id],
-                op_logs: [:id, :status, :changed_status, :content, :admin_user, :inserted_at] ]),
-              where: o.id ==^order_id,
-              preload: [user: user, details: details, op_logs: op_logs]
+    query = 
+      from o in MallOrder,
+        left_join: details in assoc(o, :details),
+        left_join: user in assoc(o, :user),
+        left_join: op_logs in assoc(o, :op_logs),
+        select: map(o, [:id, :goods_name, :status, :price, :final_price, :address, :currency, :paid_type, :transaction_id, :postage, :inserted_at, :snapshots,
+          user: [:id, :nickname, :mobile, :email],
+          details: [:id, :goods_name, :goods_pic, :price, :amount, :mall_goods_id],
+          op_logs: [:id, :status, :changed_status, :content, :admin_user, :inserted_at] ]),
+        where: o.id ==^order_id,
+        preload: [user: user, details: details, op_logs: op_logs]
     Repo.one(query)
   end
 
@@ -161,7 +165,7 @@ defmodule Acs.MallOrderController do
 
               {:ok, mall_order_detail} = MallOrderDetail.changeset(%MallOrderDetail{}, order_detail) |> Repo.insert
             end)
-            conn |>json(%{success: true, order_id: order_id, i18n_message: "mall.order.addSuccess"})
+            conn |> json(%{success: true, order_id: order_id, i18n_message: "mall.order.addSuccess"})
           end
         end
       else
@@ -182,35 +186,38 @@ defmodule Acs.MallOrderController do
   end
 
   def fetch_my_orders(%Plug.Conn{private: %{acs_session_user_id: user_id}} = conn,
-      %{"type" => type, "page" => page, "records_per_page" => records_per_page}) 
+      %{"type" => type, "page" => page, "records_per_page" => records_per_page})
       when type in ["all","waitPay","waitConfirm"] do
     fetch_my_orders(conn,user_id, type, page, records_per_page)
   end
   def fetch_my_orders(conn, _params) do
-    conn |>json(%{success: false, i18n_message: "mall.order.messages.illegal"})
+    conn |> json(%{success: false, i18n_message: "mall.order.messages.illegal"})
   end
   defp fetch_my_orders(conn, user_id, type, page, records_per_page) do
     total = Repo.one!(from order in MallOrder, select: count(1))
     total_page = round(Float.ceil(total / records_per_page))
 
-    query = from order in MallOrder,
-              left_join: details in assoc(order, :details),
-              left_join: user in assoc(order, :user),
-              select: map(order, [:id, :goods_name, :status, :price, :final_price, :currency, :postage, :inserted_at,
-                user: [:id, :nickname, :mobile],
-                details: [:id, :goods_name, :goods_pic, :price, :amount] ]),
-              order_by: [desc: order.inserted_at],
-              limit: ^records_per_page,
-              offset: ^((page - 1) * records_per_page),
-              preload: [user: user, details: details]
-      query = case type  do
-              "waitPay" ->
-                 query |> where([o], o.status == 0)
-              "waitConfirm" ->
-                 query |> where([o], o.status in [1,2])
-                _ ->
-                 query
-              end
+    query = 
+      from order in MallOrder,
+        left_join: details in assoc(order, :details),
+        left_join: user in assoc(order, :user),
+        select: map(order, [:id, :goods_name, :status, :price, :final_price, :currency, :postage, :inserted_at,
+          user: [:id, :nickname, :mobile],
+          details: [:id, :goods_name, :goods_pic, :price, :amount] ]),
+        order_by: [desc: order.inserted_at],
+        limit: ^records_per_page,
+        offset: ^((page - 1) * records_per_page),
+        preload: [user: user, details: details]
+
+      query =
+        case type  do
+          "waitPay" ->
+              query |> where([o], o.status == 0)
+          "waitConfirm" ->
+              query |> where([o], o.status in [1,2])
+            _ ->
+              query
+        end
     orders = Repo.all(query)
     conn |> json(%{success: true, orders: orders, total: total_page})
   end
@@ -219,49 +226,50 @@ defmodule Acs.MallOrderController do
     order = Repo.get!(MallOrder, order_id)
     cond  do
       not order.status in [1,2]  ->
-        conn |>json(%{success: false, i18n_message: "mall.order.messages.repeatRecieved"})
+        conn |> json(%{success: false, i18n_message: "mall.order.messages.repeatRecieved"})
       order.user_id != user_id ->
-        conn |>json(%{success: false, i18n_message: "mall.order.messages.illegal"})
+        conn |> json(%{success: false, i18n_message: "mall.order.messages.illegal"})
       true ->
         finish_status = 4
         from( od in MallOrder, where: od.id == ^order.id) |> Repo.update_all( set: [status: finish_status])
         new_order = Map.put(order, :status, finish_status)
-        conn |>json(%{success: true, order: new_order, i18n_message: "mall.order.messages.recievedSuccess"})
+        conn |> json(%{success: true, order: new_order, i18n_message: "mall.order.messages.recievedSuccess"})
     end
   end
 
   def update_order_payed(conn, %{"order_id" => "", "transaction_id" => ""}) do
-    conn |>json(%{success: false, i18n_message: "mall.order.messages.illegal"})
+    conn |> json(%{success: false, i18n_message: "mall.order.messages.illegal"})
   end
   def update_order_payed(conn, %{"order_id" => order_id, "transaction_id" => transaction_id}) do
     order = fetch_order_info(order_id)
     payed_status = 1
 
     if order.status !=0 and order.status != -1 do
-      conn |>json(%{success: false, i18n_message: "admin.mall.order.messages.onlyCancelOrUnpay"})
+      conn |> json(%{success: false, i18n_message: "admin.mall.order.messages.onlyCancelOrUnpay"})
     else
-      result= Repo.transaction(fn ->
-              Enum.each(order.details, fn(detail) ->
-                goods = Repo.get(MallGoods, detail.mall_goods_id)
-                if detail.amount>goods.stock do
-                  Repo.rollback("admin.mall.order.messages.stockOut")
-                else
-                  MallGoods.changeset(goods, %{stock: goods.stock - detail.amount, sold: goods.sold + detail.amount}) |> Repo.update()
-                  RedisMall.refresh(goods)
-                end
-              end)
+      result = 
+        Repo.transaction(fn ->
+          Enum.each(order.details, fn(detail) ->
+            goods = Repo.get(MallGoods, detail.mall_goods_id)
+            if detail.amount>goods.stock do
+              Repo.rollback("admin.mall.order.messages.stockOut")
+            else
+              MallGoods.changeset(goods, %{stock: goods.stock - detail.amount, sold: goods.sold + detail.amount}) |> Repo.update()
+              RedisMall.refresh(goods)
+            end
+          end)
 
-              from( od in MallOrder, where: od.id == ^order.id) |> Repo.update_all( set: [status: payed_status ,transaction_id: transaction_id])
-              MallOPLog.changeset(%MallOPLog{},%{ mall_order_id: order_id,  status: order.status, changed_status: payed_status, content: %{ transaction_id: transaction_id} }) |> Repo.insert
-            end)
+          from( od in MallOrder, where: od.id == ^order.id) |> Repo.update_all( set: [status: payed_status ,transaction_id: transaction_id])
+          MallOPLog.changeset(%MallOPLog{},%{ mall_order_id: order_id,  status: order.status, changed_status: payed_status, content: %{ transaction_id: transaction_id} }) |> Repo.insert
+        end)
 
       case result do
         {:error, i18n_message} ->
-          conn |>json(%{success: false, i18n_message: i18n_message})
+          conn |> json(%{success: false, i18n_message: i18n_message})
         _ ->
           Elasticsearch.update(%{ index: "mall", type: "orders", doc: %{ doc: %{ transaction_id: transaction_id}}, params: nil, id: order_id})
           order = fetch_order_info(order_id)
-          conn |>json(%{success: true, order: order, i18n_message: "admin.mall.order.messages.opSuccess"})
+          conn |> json(%{success: true, order: order, i18n_message: "admin.mall.order.messages.opSuccess"})
       end
     end
   end
@@ -272,27 +280,28 @@ defmodule Acs.MallOrderController do
 
     cond do
       refund_free > order.final_price ->
-        conn |>json(%{success: false, i18n_message: "admin.mall.order.messages.refundMoneyOut"})
+        conn |> json(%{success: false, i18n_message: "admin.mall.order.messages.refundMoneyOut"})
       order.status !=2 ->
-        conn |>json(%{success: false, i18n_message: "admin.mall.order.messages.onlyRecieving"})
+        conn |> json(%{success: false, i18n_message: "admin.mall.order.messages.onlyRecieving"})
       true ->
         cancel_status = -1
-        result= Repo.transaction(fn ->
-                Enum.each(order.details, fn(detail) ->
-                  goods = Repo.get(MallGoods, detail.mall_goods_id)
-                  MallGoods.changeset(goods, %{stock: goods.stock + detail.amount, sold: goods.sold - detail.amount}) |> Repo.update()
-                  RedisMall.refresh(goods)
-                end)
+        result = 
+          Repo.transaction(fn ->
+            Enum.each(order.details, fn(detail) ->
+              goods = Repo.get(MallGoods, detail.mall_goods_id)
+              MallGoods.changeset(goods, %{stock: goods.stock + detail.amount, sold: goods.sold - detail.amount}) |> Repo.update()
+              RedisMall.refresh(goods)
+            end)
 
-                from( od in MallOrder, where: od.id == ^order.id) |> Repo.update_all( set: [status: cancel_status])
-                MallOPLog.changeset(%MallOPLog{},%{ mall_order_id: order_id,  status: order.status, changed_status: cancel_status, content: %{ refund_money: refund_free} }) |> Repo.insert
-              end)
+            from( od in MallOrder, where: od.id == ^order.id) |> Repo.update_all( set: [status: cancel_status])
+            MallOPLog.changeset(%MallOPLog{},%{ mall_order_id: order_id,  status: order.status, changed_status: cancel_status, content: %{ refund_money: refund_free} }) |> Repo.insert
+         end)
         case result do
           {:error, i18n_message} ->
-            conn |>json(%{success: false, i18n_message: i18n_message})
+            conn |> json(%{success: false, i18n_message: i18n_message})
           _ ->
             order = fetch_order_info(order_id)
-            conn |>json(%{success: true, order: order, i18n_message: "admin.mall.order.messages.opSuccess"})
+            conn |> json(%{success: true, order: order, i18n_message: "admin.mall.order.messages.opSuccess"})
         end
     end
   end

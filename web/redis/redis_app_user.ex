0dce8c1ea9abf757f2953f6b6bf88f525d767b2a
xiaobin <xiaobin@firevale.com>
2017-05-24 16:06:09 +0800

    wip

--- web/redis/redis_app_user.ex
+++ web/redis/redis_app_user.ex
@@ -16,27 +16,21 @@ defmodule Acs.RedisAppUser do
     Cachex.get!(:mem_cache, key, fallback: fn(redis_key) -> 
       case Redis.get(redis_key) do 
         :undefined ->
-          refresh(app_id, zone_id, user_id)
+          case StatsRepo.get_by(AppUser, app_id: app_id, user_id: user_id, zone_id: zone_id) do 
+            nil ->  
+              {:ok, appUser} = AppUser.changeset(%AppUser{}, %{app_id: app_id, user_id: user_id, zone_id: zone_id}) |> StatsRepo.insert
+              Redis.setex(key, 3600 * 24, appUser |> :erlang.term_to_binary |> Base.encode64)
+              appUser
+
+            %AppUser{} = appUser ->
+              Redis.setex(key, 3600 * 24, appUser |> :erlang.term_to_binary |> Base.encode64)
+              appUser
+          end
 
         raw -> 
           raw |> Base.decode64! |> :erlang.binary_to_term
       end
     end)
-  end
-
-  def refresh(app_id, zone_id, user_id) do 
-    key = "#{@app_user_cache_key}.#{app_id}.#{zone_id}.#{user_id}"
-    Cachex.del(:mem_cache, key)
-    case StatsRepo.get_by(AppUser, app_id: app_id, user_id: user_id, zone_id: zone_id) do 
-      nil ->  
-        {:ok, appUser} = AppUser.changeset(%AppUser{}, %{app_id: app_id, user_id: user_id, zone_id: zone_id}) |> StatsRepo.insert
-        Redis.setex(key, 3600 * 24, appUser |> :erlang.term_to_binary |> Base.encode64)
-        appUser
-
-      %AppUser{} = appUser ->
-        Redis.setex(key, 3600 * 24, appUser |> :erlang.term_to_binary |> Base.encode64)
-        appUser
-    end
   end
 
 end

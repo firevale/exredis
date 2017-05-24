1a220b251d23e4031c8df994f90146c0e7e36036
zhongxb <zhongxb@firevale.com>
2017-05-24 09:52:54 +0800

    wip

--- test/models/user_address_test.exs
+++ test/models/user_address_test.exs
@@ -3,8 +3,10 @@ defmodule Acs.UserAddressTest do
 
   alias Acs.UserAddress
 
-  @valid_attrs %{name: "some content"}
-  @invalid_attrs %{}
+  @valid_attrs %{name: "flanker", mobile: "18959118888", area: "fuzhou", address: "wuyilu",
+                area_code: "350100", is_default: true, user_id: 100001}
+  @invalid_attrs %{name: "flanker", mobile: "12959118888", area: "fuzhou", address: "wuyilu",
+                area_code: "350100", is_default: true, user_id: 1}
 
   test "changeset with valid attributes" do
     changeset = UserAddress.changeset(%UserAddress{}, @valid_attrs)

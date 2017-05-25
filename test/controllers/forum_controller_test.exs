9c3220be8c49dd731117ff4e4a5b99dd4a385139
zhongxb <zhongxb@firevale.com>
2017-05-25 18:12:46 +0800

    wip

--- test/controllers/forum_controller_test.exs
+++ test/controllers/forum_controller_test.exs
@@ -4,6 +4,7 @@ defmodule Acs.ForumControllerTest do
   alias Acs.App
   alias Acs.Repo
   alias Acs.User
+  alias Acs.AdminSetting
   alias Acs.AdminUser
   alias Acs.RedisUser
   alias Acs.Forum
@@ -14,20 +15,21 @@ defmodule Acs.ForumControllerTest do
   alias Acs.RedisAccessToken
 
   alias Utils.JSON
-
   require Utils
 
+  @app_id   "978A7D84040FE589ED0C76295131E43D"
+  @user_id  100001
+
   setup %{conn: conn} = tags do
-      app_id = "978A7D84040FE589ED0C76295131E43D"
       device_id = "idfa.#{Utils.generate_token()}"
       platform = "ios"
 
       conn = conn
         |> recycle()
-        |> set_acs_header(app_id, device_id, platform)
+        |> set_acs_header(device_id, platform)
 
       app = App.changeset(%App{}, %{
-            id: app_id,
+            id: @app_id,
             secret: Utils.generate_token(),
             name: "Test App",
             currency: "CNY",
@@ -35,7 +37,7 @@ defmodule Acs.ForumControllerTest do
           }) |> Repo.insert!(on_conflict: :nothing)
 
       user = User.changeset(%User{}, %{
-        id: 100001,
+        id: @user_id,
         email: "zhongxiaobin@firevale.com",
         encrypted_password: "$pbkdf2-sha512$160000$XBC9izsPHnce3kqllIpE8A$xh0TY1uYF3VKukY5fwyqsGEkLqvY.o.iIdaN536i2lSJp6Bnu4xNsu/FH243xuEv9UrLQXLOPmPetmi3hrmdmA"
         }) |> Repo.insert!(on_conflict: :nothing)
@@ -46,7 +48,7 @@ defmodule Acs.ForumControllerTest do
         account_id: user.email,
         active: true,
         admin_level: 1,
-        user_id: user.id
+        user_id: @user_id
         }) |> Repo.insert!(on_conflict: :nothing)
 
       resp = post(conn, "/user/create_token", %{
@@ -66,47 +68,46 @@ defmodule Acs.ForumControllerTest do
       {:ok, %{tags | conn: conn}}
     end
 
-  defp set_acs_header(conn, app_id, device_id, platform) do
-    conn |> put_private(:acs_app_id, app_id)
+  defp set_acs_header(conn, device_id, platform) do
+    conn |> put_private(:acs_app_id, @app_id)
          |> put_private(:acs_platform, platform)
          |> put_req_header("acs-locale", "zh-Hans")
          |> put_req_header("acs-zone-id", "1")
-         |> put_req_header("acs-app-id", app_id)
+         |> put_req_header("acs-app-id", @app_id)
          |> put_private(:acs_device_id, device_id)
-         |> put_private(:acs_session_user_id, 100001)
+         |> put_private(:acs_session_user_id, @user_id)
   end
 
   test "update forum info", context do
-
     forum = Forum.changeset(%Forum{}, %{
         title: "orig title",
         active: true,
         icon: "orig icon",
-        app_id: "978A7D84040FE589ED0C76295131E43D"
+        app_id: @app_id
         }) |> Repo.insert!(on_conflict: :nothing)
 
     resp = post(context.conn, "/admin_actions/forum/update_forum_info", %{
       "forum" => %{
-        "id" => 1,
+        "id" => forum.id,
         "title" => "test",
         "active" => true,
         "icon" => "test",
-        "app_id" => "978A7D84040FE589ED0C76295131E43D"
+        "app_id" => @app_id
       } })
 
     result = JSON.decode!(resp.resp_body, keys: :atoms)
+    IO.inspect resp.resp_body
 
     assert resp.status == 200
     assert result.success
   end
 
   test "update section info", context do
-    
     forum = Forum.changeset(%Forum{}, %{
         title: "orig title",
         active: true,
         icon: "orig icon",
-        app_id: "978A7D84040FE589ED0C76295131E43D"
+        app_id: @app_id
         }) |> Repo.insert!(on_conflict: :nothing)
 
     section = ForumSection.changeset(%ForumSection{}, %{
@@ -126,45 +127,82 @@ defmodule Acs.ForumControllerTest do
       } })
 
     result = JSON.decode!(resp.resp_body, keys: :atoms)
+    IO.inspect resp.resp_body
 
     assert resp.status == 200
     assert result.success
   end
 
-  # test "get forum info with keyword", context do
-  #   resp = post(context.conn, "/forum_actions/get_forum_info_with_keyword", %{
-  #     forum_id: 1
-  #   })
+  test "get forum info with keyword", context do
+    forum = Forum.changeset(%Forum{}, %{
+        title: "orig title",
+        active: true,
+        icon: "orig icon",
+        app_id: @app_id
+        }) |> Repo.insert!(on_conflict: :nothing)
 
-  #   IO.inspect resp.resp_body
-  #   # result = JSON.decode!(resp.resp_body, keys: :atoms)
-  #   # IO.inspect result
+    setting = AdminSetting.changeset(%AdminSetting{}, %{
+        name: "keyword",
+        value: "keyword",
+        active: true,
+        group: "keyword",
+        memo: "keyword"
+        }) |> Repo.insert!(on_conflict: :nothing)
 
-  #   assert resp.status == 200
-  #   # assert result.success
-  # end
+    resp = post(context.conn, "/forum_actions/get_forum_info_with_keyword", %{
+      forum_id: forum.id
+    })
 
-  # test "get user paged post", context do
-  #   resp = post(context.conn, "/forum_actions/get_user_paged_post", %{
-  #     forum_id: 1,
-  #     page: 1,
-  #     records_per_page: 10
-  #   })
+    result = JSON.decode!(resp.resp_body, keys: :atoms)
+    IO.inspect resp.resp_body
 
-  #   IO.inspect resp.resp_body
-  #   # result = JSON.decode!(resp.resp_body, keys: :atoms)
-  #   # IO.inspect result
+    assert resp.status == 200
+    assert result.success
+  end
 
-  #   assert resp.status == 200
-  #   # assert result.success
+  test "get user paged post", context do
+    resp = post(context.conn, "/forum_actions/get_user_paged_post", %{
+      forum_id: 1,
+      page: 1,
+      records_per_page: 10
+    })
 
-  # end
+    result = JSON.decode!(resp.resp_body, keys: :atoms)
+    IO.inspect resp.resp_body
 
-  # test "add post" do
-    
-    
+    assert resp.status == 200
+    assert result.success
+  end
 
-  # end
+  test "add post", context do
+    forum = Forum.changeset(%Forum{}, %{
+        title: "orig title",
+        active: true,
+        icon: "orig icon",
+        app_id: @app_id
+        }) |> Repo.insert!(on_conflict: :nothing)
+
+    section = ForumSection.changeset(%ForumSection{}, %{
+        title: "orig title",
+        active: true,
+        sort: 1,
+        forum_id: forum.id
+        }) |> Repo.insert!(on_conflict: :nothing)
+
+    resp = post(context.conn, "/forum_actions/add_post", %{
+      forum_id: forum.id,
+      section_id: section.id,
+      title: "test title",
+      content: "test content"
+    })
+
+    result = JSON.decode!(resp.resp_body, keys: :atoms)
+    IO.inspect resp.resp_body
+
+    assert resp.status == 200
+    assert result.success
+
+  end
 
   # test "get post detail" do
     

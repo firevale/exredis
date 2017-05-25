60efba217d4b4b5b7dbf12e76ad258d38899f714
xiebing <xiebing@firevale.com>
2017-05-25 12:07:34 +0800

    wip

--- static/app/views/forum/newComment.vue
+++ static/app/views/forum/newComment.vue
@@ -10,16 +10,16 @@
         <h6 class="title is-6" style="font-weight: 400">{{$t('forum.detail.replyBtn') + ": " + currentPostTitle}}</h6>
       </div>
       <form class="post" @submit.prevent="handleSubmit" v-show="currentPostTitle">
-        <quill-editor v-model="content" :placeholder="$t('forum.newPost.textAreaPlaceHolder')" @ready="setEditor" @input="handleValidation($v.content)"
-          @image="onInsertImage">
+        <quill-editor v-model="content" :placeholder="$t('forum.newPost.textAreaPlaceHolder')" @ready="setEditor"
+          @input="handleValidation($v.content)" @image="onInsertImage">
         </quill-editor>
         <div class="tile is-full has-text-left" style="margin-top: 0.5rem" v-show="errorHint">
           <span class="icon is-sign">!</span>
           <span class="is-primary" style="font-size: 1rem">{{errorHint}}</span>
         </div>
         <div class="tile is-full has-text-centered" style="justify-content: center; margin-top: 0.5rem">
-          <input type="submit" :value="$t('forum.newPost.btnTitle')" class="button is-info" :disabled="processing" :class="processing ? 'is-disabled' : ''"
-          />
+          <input type="submit" :value="$t('forum.writeComment.title')" class="button is-info" :disabled="processing"
+            :class="processing ? 'is-disabled' : ''" />
         </div>
       </form>
     </scroller>
@@ -143,11 +143,13 @@ export default {
         menu.$on('item-selected', (item) => {
           switch (item.value) {
             case 'camera':
-              nativeApi.pickImageFrom('camera', result => this.handlePickImageResult(editor, result))
+              nativeApi.pickImageFrom('camera', result => this.handlePickImageResult(editor,
+                result))
               break;
 
             case 'photoLib':
-              nativeApi.pickImageFrom('photoLib', result => this.handlePickImageResult(editor, result))
+              nativeApi.pickImageFrom('photoLib', result => this.handlePickImageResult(editor,
+                result))
               break;
           }
         })
@@ -216,8 +218,9 @@ export default {
         let postId = this.$route.params.postId
         let result = await this.$acs.addComment({
           comment_id: this.id,
-          post_id: postId, 
-          content: this.content})
+          post_id: postId,
+          content: this.content
+        })
         if (result.success) {
           Toast.show(this.$t('forum.writeComment.addSuccess'))
           this.$router.replace({

7b3277c56858d4ce0ccb95ee6f9214d28e6d64af
zhongxb <zhongxb@firevale.com>
2017-05-31 14:39:34 +0800

    wip

---
+++ test/lib/redis_admin_user_test.exs
@@ -0,0 +1,28 @@
+defmodule Acs.RedisAdminUserTest do
+  use Acs.ModelCase
+
+  require Redis
+  alias   Acs.RedisAdminUser
+
+  @user_id  100001
+
+  test "RedisAdminUser" do
+
+    admin_level = RedisAdminUser.get_admin_level(@user_id)
+    assert admin_level >= 0 
+    IO.inspect admin_level
+
+    if(admin_level > 1) do
+      admin_appids = RedisAdminUser.get_admin_appids(@user_id)
+      assert length(admin_appids) >= 1 
+    end
+
+    admin_level = RedisAdminUser.refresh(@user_id, nil, true)
+    assert admin_level >= 0 
+    IO.inspect admin_level
+
+  end
+
+
+
+end

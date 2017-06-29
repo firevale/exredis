3c9a4b4350ad588039499334cb699265b985b958
zhongxb <zhongxb@firevale.com>
2017-06-29 16:06:35 +0800

    wip

---
+++ lib/acs/web/controllers/admin/admin_wcp_controller.ex
@@ -0,0 +1,11 @@
+defmodule Acs.Web.AdminWcpController do
+  use Acs.Web, :controller
+
+  alias Acs.AppWcpConfigs
+  alias Acs.AppWcpConfigs
+  alias Acs.AppWcpConfigs
+  # alias Acs.RedisSetting
+
+  
+
+end

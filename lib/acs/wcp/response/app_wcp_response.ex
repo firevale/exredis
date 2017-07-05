a697e92e2eeaffcea63978281c6700ab3d46a6bf
xiaobin <xiaobin@firevale.com>
2017-07-05 12:29:02 +0800

    wip

--- lib/acs/wcp/response/app_wcp_response.ex
+++ lib/acs/wcp/response/app_wcp_response.ex
@@ -1,38 +1,47 @@
 defmodule Acs.AppWcpResponse do 
-  use LogAlias
+  use   LogAlias
+  alias Acs.RedisApp
   alias Acs.WcpLoginCodeResponse
   alias Acs.AppWcpConfig
   alias Acs.RedisAppWcpConfig
   alias Acs.RedisAppWcpMessageRule
 
   def build_reply(app_id, %{msgtype: "event"} = msg) do 
-    case RedisAppWcpConfig.find(app_id) do 
-      %AppWcpConfig{} = wcp_cfg ->
-        _build_event_reply(app_id, msg, wcp_cfg)
-      _ -> 
-        nil
+    case RedisApp.find(app_id) do 
+      nil -> nil
+      %{} ->
+        case RedisAppWcpConfig.find(app_id) do 
+          %AppWcpConfig{} = wcp_cfg ->
+            _build_event_reply(app_id, msg, wcp_cfg)
+          _ -> 
+            nil
+        end
     end
   end
   def build_reply(app_id, %{msgtype: "text"} = msg) do 
-    case RedisAppWcpMessageRule.get_all(app_id) do 
-      nil ->
-        _build_default_text_reply(app_id, msg)
-
-      rules ->
-        case Map.get(rules, msg.content) do 
+    case RedisApp.find(app_id) do 
+      nil -> nil
+      %{} ->
+        case RedisAppWcpMessageRule.get_all(app_id) do 
           nil ->
             _build_default_text_reply(app_id, msg)
-          
-          ":login_code" -> 
-            %{from: msg.tousername, 
-              to: msg.fromusername, 
-              content: WcpLoginCodeResponse.build_reply_content(app_id, msg.fromusername)}
-            
-          response when is_bitstring(response) ->
-            %{from: msg.tousername, to: msg.fromusername, content: response}
 
-          _ -> 
-            nil
+          rules ->
+            case Map.get(rules, msg.content) do 
+              nil ->
+                _build_default_text_reply(app_id, msg)
+              
+              ":login_code" -> 
+                %{from: msg.tousername, 
+                  to: msg.fromusername, 
+                  content: WcpLoginCodeResponse.build_reply_content(app_id, msg.fromusername)}
+                
+              response when is_bitstring(response) ->
+                %{from: msg.tousername, to: msg.fromusername, content: response}
+
+              _ -> 
+                nil
+            end
         end
     end
   end
--- lib/acs/wcp/response/login_code_response.ex
+++ lib/acs/wcp/response/login_code_response.ex
@@ -2,6 +2,7 @@ defmodule Acs.WcpLoginCodeResponse do
   alias Acs.AppWcpConfig
   alias Acs.RedisAppWcpConfig
   alias Acs.AppLoginCode
+  alias Acs.RedisApp 
 
   alias   Acs.Repo
   import  Ecto.Query
@@ -9,34 +10,41 @@ defmodule Acs.WcpLoginCodeResponse do
   alias   Ecto.Adapters.SQL
 
   def build_reply_content(app_id, from) do 
-    case RedisAppWcpConfig.find(app_id) do 
-      %AppWcpConfig{} = cfg ->
-        owner = "openid.#{from}"
-        query = from c in AppLoginCode,
-                  select: c,
-                  where: c.owner == ^owner
+    case RedisApp.find(app_id) do 
+      %{} = app
+        case RedisAppWcpConfig.find(app_id) do 
+          %AppWcpConfig{} = cfg ->
+            if app.can_assign_code do 
+              owner = "openid.#{from}"
+              query = from c in AppLoginCode,
+                        select: c,
+                        where: c.owner == ^owner
 
-        case Repo.one(query) do 
-          %AppLoginCode{code: code, owner: ^owner} ->
-            _build_content(cfg.owned_code_template, code)
-          
-          _ ->
-            case SQL.query(Repo, "update app_login_codes set owner = ?, \
-                                  assigned_at = ? \
-                                  where app_id = ? \
-                                  and owner is null and user_id is null \
-                                  order by inserted_at limit ?", [owner, DateTime.utc_now(), app_id, 1]) do 
+              case Repo.one(query) do 
+                %AppLoginCode{code: code, owner: ^owner} ->
+                  _build_content(cfg.owned_code_template, code)
+                
+                _ ->
+                  case SQL.query(Repo, "update app_login_codes set owner = ?, \
+                                        assigned_at = ? \
+                                        where app_id = ? \
+                                        and owner is null and user_id is null \
+                                        order by inserted_at limit ?", [owner, DateTime.utc_now(), app_id, 1]) do 
 
-              {:ok, %{num_rows: 1}} -> 
-                %{code: code} = Repo.one(query) 
-                _build_content(cfg.new_code_template, code)
-            
-              _ ->
-                cfg.no_code_template || "所有激活码已全部发放完成."
+                    {:ok, %{num_rows: 1}} -> 
+                      %{code: code} = Repo.one(query) 
+                      _build_content(cfg.new_code_template, code)
+                  
+                    _ ->
+                      cfg.no_code_template || "所有激活码已全部发放完成(默认回复，请在后台编辑此消息)"
+                  end
+              end
+            else 
+              cfg.closed_template || "激活码领取未开启(默认回复，请在后台编辑本消息)"
             end
+          _ -> nil
         end
-      _ -> 
-        nil
+      _ -> nil
     end
   end
 

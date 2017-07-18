766003b8bbbe721eb28a7f78a84c68de2ff68291
xiaobin <xiaobin@firevale.com>
2017-07-18 14:21:28 +0800

    wip

--- lib/acs/web/controllers/admin/chart_controller.ex
+++ lib/acs/web/controllers/admin/chart_controller.ex
@@ -8,28 +8,38 @@ defmodule Acs.Web.Admin.ChartController do
     with all <- Redis.lrange("onlines.#{app_id}", 0, n), 
          ios <- Redis.lrange("ponlines.#{app_id}.ios", 0, n),
          android <- Redis.lrange("ponlines.#{app_id}.android", 0, n) do 
-      all = Enum.reverse(all)
-      ios = Enum.reverse(ios)
-      android = Enum.reverse(android)
 
-      labels = Enum.map(all, fn(x) ->
+      {labels, keys} = Enum.reduce(all, {[], []}, fn(x, {l, k}) ->
         [ts, _counter] = String.split(x, ".", trim: true)
-        ts |> String.to_integer |> Timex.from_unix |> Timex.Timezone.convert(tz) |> Timex.format!("{0M}-{D} {h24}:{0m}")
+        label = ts |> String.to_integer |> Timex.from_unix |> Timex.Timezone.convert(tz) |> Timex.format!("{0M}-{D} {h24}:{0m}")
+        {[label | l], [ts | k]}
       end)   
 
-      all_data = Enum.map(all, fn(x) -> 
-        [_ts, counter] = String.split(x, ".", trim: true)
-        String.to_integer(counter)
+      all = Enum.map(all |> Enum.reverse, fn(x) -> 
+        [k, v] = String.split(x, ".", trim: true)  
+        {k, String.to_integer(v)}
+      end) |> Enum.into(%{})
+
+      ios = Enum.map(ios |> Enum.reverse, fn(x) -> 
+        [k, v] = String.split(x, ".", trim: true)  
+        {k, String.to_integer(v)}
+      end) |> Enum.into(%{})
+
+      android = Enum.map(android |> Enum.reverse, fn(x) -> 
+        [k, v] = String.split(x, ".", trim: true)  
+        {k, String.to_integer(v)}
+      end) |> Enum.into(%{})
+
+      all_data = Enum.map(keys, fn(k) -> 
+        Map.get(all, k, 0)
       end)
 
-      ios_data = Enum.map(ios, fn(x) -> 
-        [_ts, counter] = String.split(x, ".", trim: true)
-        String.to_integer(counter)
+      ios_data = Enum.map(keys, fn(k) -> 
+        Map.get(ios, k, 0)
       end)
 
-      android_data = Enum.map(android, fn(x) -> 
-        [_ts, counter] = String.split(x, ".", trim: true)
-        String.to_integer(counter)
+      android_data = Enum.map(keys, fn(k) -> 
+        Map.get(android, k, 0)
       end)
 
       conn |> json(%{

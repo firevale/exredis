48702020b43f743f6ff141a1ed903e3cb62b0ea1
xiaobin <xiaobin@firevale.com>
2017-07-21 10:38:37 +0800

    wip

---
+++ lib/acs/web/controllers/admin/stats_controller.ex
@@ -0,0 +1,56 @@
+defmodule Acs.Web.Admin.StatsController do
+  use Acs.Web, :controller
+
+  def onlines(conn, %{"app_id" => app_id}) do 
+    chart = Cachex.get!(:default, "onlines_chart.#{app_id}", fallback: fn(key) -> 
+      case Redis.get(key) do 
+        :undefined ->
+          {:commit,  %{ labels: [],
+                        datasets: [
+                          %{
+                            label: "同时在线",
+                            data: [],
+                          },
+                          %{
+                            label: "同时在线(IOS)",
+                            data: [],
+                          },
+                          %{
+                            label: "同时在线(ANDROID)",
+                            data: [],
+                          }
+                        ]}}
+        raw ->
+          {:commit, raw |> Base.decode64! |> :erlang.binary_to_term}
+      end
+    end)
+    conn |> json(%{ success: true, chart: chart})
+  end
+
+  def historic_onlines(conn, %{"app_id" => app_id}) do 
+    chart = Cachex.get!(:default, "hourly_onlines_chart.#{app_id}", fallback: fn(key) -> 
+      case Redis.get(key) do 
+        :undefined ->
+          {:commit,  %{ labels: [],
+                        datasets: [
+                          %{
+                            label: "同时在线",
+                            data: [],
+                          },
+                          %{
+                            label: "同时在线(IOS)",
+                            data: [],
+                          },
+                          %{
+                            label: "同时在线(ANDROID)",
+                            data: [],
+                          }
+                        ]}}
+        raw ->
+          {:commit, raw |> Base.decode64! |> :erlang.binary_to_term}
+      end
+    end)
+    conn |> json(%{ success: true, chart: chart})
+  end
+
+end

a954ce83a75f9f3be71b8e5a8a62b84e70947768
xiaobin <xiaobin@firevale.com>
2017-08-08 17:32:23 +0800

    wip

--- lua/rand_code.lua
+++ lua/rand_code.lua
@@ -4,17 +4,24 @@ local app_id = KEYS[1]
 local open_id = ARGV[1]
 local key_a = '_acs.login_codes.available.'..app_id 
 local key_b = '_acs.login_codes.assigned.'..app_id
+local key_owns = '_acs.login_codes.owns.'..app_id..'.'..open_id 
 
-local code = redis.call('srandmember', key_a)
+local owns = redis.call('get', key_owns)
 
-if code then
-  redis.call('smove', key_a, key_b, code)
-else 
-  code = 'undefined'
+if owns then 
+  return owns  
+else
+  local code = redis.call('srandmember', key_a)
+
+  if code then
+    redis.call('smove', key_a, key_b, code)
+  else 
+    code = 'undefined'
+  end
+
+  redis.call('set', key_owns, code)
+
+  return code 
 end
-
-redis.call('set', '_acs.login_codes.owns.'..app_id..open_id, code)
-
-return code 
 
 

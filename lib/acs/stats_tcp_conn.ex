9c01e88065381848909ba126165f34d31ff140ff
xiaobin <xiaobin@firevale.com>
2017-08-10 21:26:29 +0800

    add stats tcp server

---
+++ lib/acs/stats_tcp_conn.ex
@@ -0,0 +1,43 @@
+defmodule Acs.StatsTcpConn do
+  use GenServer
+  use LogAlias
+
+  def set_socket(pid, socket) do
+    GenServer.call(pid, {:set_socket, socket})
+  end
+
+
+  @doc """
+  Starts the registry.
+  """
+  def start_link() do
+    GenServer.start(__MODULE__, [])
+  end
+
+  def init(_) do
+    {:ok, %{socket: nil}}
+  end
+
+  def handle_call({:set_socket, socket}, _from, state) do 
+    :ok = :inet.setopts(socket, [:binary, packet: :line, active: :once])
+    {:reply, :ok, %{state | socket: socket}}
+  end
+
+  def handle_info({:tcp, _socket, bin}, %{socket: socket} = state) do
+    :inet.setopts(socket, active: :once)
+    {:noreply, state}
+  end
+  def handle_info({:tcp_closed, _socket}, state) do
+    {:stop, :normal, state}
+  end
+  def handle_info(info, state) do
+    Logger.info "stats tcp connection receive info: #{inspect info}"
+    {:noreply, state}
+  end
+
+  def terminate(reason, %{socket: socket}) do
+    :gen_tcp.close(socket)
+    :ok
+  end
+
+end

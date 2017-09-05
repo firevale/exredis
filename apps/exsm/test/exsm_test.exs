dbe8ab0d96159522c720dfcd4b79653b8a105b7c
xiaobin <xiaobin@firevale.com>
2017-09-05 19:02:21 +0800

    wip

--- apps/exsm/test/exsm_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExsmTest do
-  use ExUnit.Case
-  doctest Exsm
-
-  test "greets the world" do
-    assert Exsm.hello() == :world
-  end
-end
--- apps/exutils/test/exutils_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExutilsTest do
-  use ExUnit.Case
-  doctest Exutils
-
-  test "greets the world" do
-    assert Exutils.hello() == :world
-  end
-end

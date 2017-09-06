b01a782ba61e545c40c09181941a62d6f706b33f
xiaobin <xiaobin@firevale.com>
2017-09-06 14:41:51 +0800

    wip

--- mix.exs
+++ mix.exs
@@ -5,6 +5,10 @@ defmodule Exsm.Mixfile do
     [
       app: :exsm,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()

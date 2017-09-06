998cd4e0e679d1a2fd0949c32e891c06a9e1e9f4
xiaobin <xiaobin@firevale.com>
2017-09-06 14:33:50 +0800

    Initial commit

---
+++ lib/sdk_apple.ex
@@ -0,0 +1,72 @@
+defmodule SDKApple do 
+  require Logger
+  use     LogAlias 
+  alias   Utils.Httpc
+  alias   Utils.JSON 
+
+  @production_url   "https://buy.itunes.apple.com/verifyReceipt"
+  @sandbox_url      "https://sandbox.itunes.apple.com/verifyReceipt"
+
+  def verify_receipt(receipt) do 
+    response = Httpc.post_json @production_url, %{"receipt-data": receipt}
+
+    if Httpc.success?(response) do 
+      response_json = JSON.decode!(response.body)
+
+      case response_json["status"] do 
+        0 -> {:ok, translate_apple_response(response_json)}
+        21002 -> verify_sandbox_receipt(receipt) # somehow 21002 also success via sandbox purchase
+        21007 -> verify_sandbox_receipt(receipt)
+        code -> # other status
+          error "verify result: #{code} invalid receipt #{inspect response_json, pretty: true}"
+          {:error, :invalid_receipt}
+      end
+    else
+      error "verify apple store receipt failed, http response: #{inspect response, pretty: true}"
+      {:error, :network}     
+    end
+  end
+
+  defp verify_sandbox_receipt(receipt) do 
+    response = Httpc.post_json @sandbox_url, %{"receipt-data": receipt} 
+
+    if Httpc.success?(response) do 
+      response_json = JSON.decode!(response.body)
+
+      case response_json["status"] do 
+        0 -> 
+          d "response_json: #{inspect response_json, pretty: true}"
+          {:ok, translate_apple_response(response_json)}
+        code -> 
+          error "verify apple store sandbox receipt failed: #{code}"
+          {:error, :invalid_receipt}
+      end
+    else 
+      error "verify apple store sandbox receipt failed, http response: #{inspect response, pretty: true}"
+      {:error, :network}
+    end
+  end
+
+  defp translate_apple_response(%{"receipt" => %{
+    "product_id" => product_id,
+    "original_transaction_id" => transaction_id}}) do 
+    %{product_id: product_id, 
+      transaction_id: transaction_id, 
+      receipt_type: "Production"}
+  end
+  defp translate_apple_response(%{"receipt" => %{
+    "product_id" => product_id,
+    "transaction_id" => transaction_id}}) do 
+    %{product_id: product_id, 
+      transaction_id: transaction_id, 
+      receipt_type: "Production"}
+  end
+  defp translate_apple_response(%{"receipt" => %{
+    "in_app" => in_app,
+    "receipt_type" => receipt_type}}) do 
+    %{in_app: in_app,
+      receipt_type: receipt_type}
+  end
+  defp translate_apple_response(_), do: %{}
+
+end
---
+++ lib/sdk_mumayi.ex
@@ -0,0 +1,54 @@
+defmodule SDKMumayi do
+
+  require Logger
+
+  alias   Utils.Httpc
+  require Utils
+  use Bitwise
+
+  @baseUrl  "http://pay.mumayi.com/user/index/validation"
+
+  def validate_session(token, uid) do 	 
+    try do 
+      response = Httpc.post_msg(@baseUrl, %{
+                                          "token" => token,
+                                          "uid" => uid
+                                          })
+
+      if Httpc.success?(response) do 
+        response.body == "success"
+      else
+        false
+      end
+    catch
+      :error, e ->
+        Logger.error "validate downjoy session exception: #{inspect e}"
+        false
+    end
+  end
+
+  def validate_payment(app_key, %{"tradeSign" => tradeSign, "orderID" => orderID}) do 
+    sign_length = String.length(tradeSign)
+    if sign_length < 14 do
+      false
+    else
+      << verifyStr :: binary-size(8), data :: binary >> = tradeSign
+      case data |> Utils.md5_sign |> :binary.part(0, 8) do 
+        ^verifyStr ->
+          << key_b :: binary-size(6), rest :: binary >> = data
+          rand_key = Utils.md5_sign(key_b <> app_key) |> String.to_char_list
+          
+          signedOrderID = Base.decode64!(rest) |> String.to_char_list |> Enum.with_index |> Enum.map_join("", fn{c, i} ->
+            << Bitwise.bxor(c,  Enum.at(rand_key, rem(i, 32))) >>
+          end)
+
+          signedOrderID == orderID
+        _ ->
+          false
+      end
+    end
+  end
+
+  def validate_payment(_, _), do: false 
+
+end

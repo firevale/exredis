572ae49c77a19651aab765fa9cf18cd13ffd51ee
xiaobin <xiaobin@firevale.com>
2017-09-06 14:41:51 +0800

    wip

--- mix.exs
+++ mix.exs
@@ -5,6 +5,10 @@ defmodule Exredis.Mixfile do
     [
       app: :exredis,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()

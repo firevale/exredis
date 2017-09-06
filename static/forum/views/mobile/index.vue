c4cc029c5f38d4d17f97dc6d503e05422a2a8b1a
Minzz <zhumingzhen@firevale.com>
2017-09-06 15:46:06 +0800

    wip

--- static/forum/views/mobile/index.vue
+++ static/forum/views/mobile/index.vue
@@ -111,14 +111,58 @@
         </div>
       </div>
     </div>
-    <div class="forum-section card is-shadowless has-divider">
+    <div class="article-section card is-shadowless has-divider">
       <header class="card-header">
         <p class="header-title">
           | 热门推荐
         </p>
       </header>
       <div class="card-content">
-        AA
+        <v-touch class="media article">
+          <figure class="media-left">
+            <p class="image">
+              <img src="https://placehold.it/312x178?text=312x178">
+            </p>
+          </figure>
+          <div class="media-content is-clipped">
+            <p class="article-title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="intro small-text text-ellipsis">
+              为什么猪女会这么打？ 第一个原因自然是：己方辅助是牛头，对方adc是寒冰，对方的腿短和己方的控多使得自己这波gank是有足够的成功几率的
+            </p>
+          </div>
+        </v-touch>
+        <v-touch class="media article">
+          <figure class="media-left">
+            <p class="image">
+              <img src="https://placehold.it/312x178?text=312x178">
+            </p>
+          </figure>
+          <div class="media-content is-clipped">
+            <p class="article-title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="intro small-text text-ellipsis">
+              为什么猪女会这么打？ 第一个原因自然是：己方辅助是牛头，对方adc是寒冰，对方的腿短和己方的控多使得自己这波gank是有足够的成功几率的
+            </p>
+          </div>
+        </v-touch>
+        <v-touch class="media article">
+          <figure class="media-left">
+            <p class="image">
+              <img src="https://placehold.it/312x178?text=312x178">
+            </p>
+          </figure>
+          <div class="media-content is-clipped">
+            <p class="article-title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="intro small-text text-ellipsis">
+              为什么猪女会这么打？ 第一个原因自然是：己方辅助是牛头，对方adc是寒冰，对方的腿短和己方的控多使得自己这波gank是有足够的成功几率的
+            </p>
+          </div>
+        </v-touch>
       </div>
     </div>
   </div>

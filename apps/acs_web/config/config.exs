257c1ce0931dc9b6834d6563604d29b15f2d6c70
xiaobin <xiaobin@firevale.com>
2017-09-06 16:13:42 +0800

    wip

--- apps/acs_web/config/config.exs
+++ apps/acs_web/config/config.exs
@@ -24,7 +24,7 @@ config :logger, :console,
   metadata: [:request_id]
 
 config :acs_web, :generators,
-  context_app: :acs_stats
+  context_app: :acs
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.

0a42ef884eded4ac466bb8247b93055293d64e6a
zhongxb <zhongxb@firevale.com>
2017-09-07 18:18:55 +0800

    wip

---
+++ apps/acs/lib/acs/search/es_mall_goods.ex
@@ -0,0 +1,12 @@
+defmodule Acs.Search.ESMallGoods do 
+  require Elasticsearch
+  alias Acs.Malls.MallGoods
+
+  def index(%MallGoods{} = goods) do 
+    
+    # todo 
+    
+  end
+
+
+end
--- apps/acs/lib/acs/search/es_user.ex
+++ apps/acs/lib/acs/search/es_user.ex
@@ -1,3 +1,15 @@
 defmodule Acs.Search.ESUser do 
+    require Elasticsearch
+    alias Acs.Accounts.User
+
+    def index(%User{} = user) do
+        Elasticsearch.index(%{
+            index: "acs",
+            type: "user",
+            doc: user,
+            params: nil,
+            id: user.id
+        })
+    end
 
 end

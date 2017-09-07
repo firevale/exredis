dc32541cee2ba5aaa513fc90c4586a1b0dabbc63
xiaobin <xiaobin@firevale.com>
2017-09-07 20:46:50 +0800

    wip

--- apps/acs/lib/acs/search/es_forum.ex
+++ apps/acs/lib/acs/search/es_forum.ex
@@ -2,7 +2,7 @@ defmodule Acs.Search.ESForum do
     require Elasticsearch
     alias Acs.Forums.ForumPost
 
-    def index(%ForumPost{} = post, title, content) do
+    def index(%ForumPost{} = post) do
       Elasticsearch.index(%{
         index: "forum",
         type: "posts",
@@ -10,17 +10,17 @@ defmodule Acs.Search.ESForum do
           forum_id: post.forum_id,
           section_id: post.section_id,
           user_id: post.user_id,
-          title: title,
-          content: content,
-          is_top: false,
-          is_hot: false,
-          is_vote: false,
-          active: true,
+          title: post.title,
+          content: post.content,
+          is_top: post.is_top,
+          is_hot: post.is_hot,
+          is_vote: post.is_vote,
+          active: post.active,
           has_pic: post.has_pic,
-          reads: 0,
-          comms: 0,
+          reads: post.reads,
+          comms: post.comms,
           inserted_at: Timex.format!(post.inserted_at, "{YYYY}-{0M}-{0D}T{h24}:{0m}:{0s}+00:00"),
-          last_reply_at: Timex.format!(post.inserted_at, "{YYYY}-{0M}-{0D}T{h24}:{0m}:{0s}+00:00"),
+          last_reply_at: Timex.format!(post.last_reply_at, "{YYYY}-{0M}-{0D}T{h24}:{0m}:{0s}+00:00"),
         },
         params: nil,
         id: post.id

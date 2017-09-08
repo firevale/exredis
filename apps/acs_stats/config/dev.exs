81ffcb8d724f20403268186c679b53e2d9f7c671
xiaobin <xiaobin@firevale.com>
2017-09-08 17:29:34 +0800

    wip

--- apps/acs_stats/config/dev.exs
+++ apps/acs_stats/config/dev.exs
@@ -1,10 +1,3 @@
 use Mix.Config
 
-# Configure your database
-config :acs_stats, AcsStats.Repo,
-  adapter: Ecto.Adapters.MySQL,
-  username: "root",
-  password: "",
-  database: "acs_dev",
-  hostname: "localhost",
-  pool_size: 10
+

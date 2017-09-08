e1696dafb4d725529160cf79ee586e7ced7681d8
xiaobin <xiaobin@firevale.com>
2017-09-08 14:58:06 +0800

    wip

--- apps/exmail/mix.exs
+++ apps/exmail/mix.exs
@@ -14,13 +14,19 @@ defmodule Exmail.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :exutils]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:bamboo, "~> 1.0.0-rc", override: true},
+      {:bamboo_smtp, "~> 1.4"},
+      {:phoenix, "~> 1.3"},
+      {:gettext, "~> 0.11"},
+
+      {:exutils, in_umbrella: true},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]

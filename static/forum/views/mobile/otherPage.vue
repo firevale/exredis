cb959541e83f737baad2a8d446c8ee885b2b47d9
xiebing <xiebing@firevale.com>
2017-09-08 14:46:47 +0800

    wip

---
+++ static/forum/views/mobile/otherPage.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="other-page">
+    <div class="comp-author-info">
+      <figure>
+        <img src="~assets/themes/jqxs_mobile/2-5_03_03.png">
+      </figure>
+      <p class="item-left subtitle">
+        firevale-城岸
+        <br/> 2017-06-28 17:56
+        <br/> 2017-06-28 17:56
+      </p>
+      <p class="item-right  has-text-bottom subtitle">回复/查看 110/1000 </p>
+    </div>
+    <div class="card has-divider">
+      <header class="card-header">
+        <p class="card-header-title">
+          发过的帖子
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="box no-radius is-marginless	is-paddingless">
+          <router-link class="article" :to="{name: 'detail', params:{ postId: 1}}" tag="div">
+            <p class="title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛阿斯顿发 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="subtitle article-date">
+              <span>2017-06-28 17:56</span>
+              <span> | </span>
+              <span>11111/11111</span>
+            </p>
+          </router-link>
+          <router-link class="article" :to="{name: 'detail', params:{ postId: 1}}" tag="div">
+            <p class="title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛阿斯顿发 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="subtitle article-date">
+              <span>2017-06-28 17:56</span>
+              <span> | </span>
+              <span>11111/11111</span>
+            </p>
+          </router-link>
+          <router-link class="article" :to="{name: 'detail', params:{ postId: 1}}" tag="div">
+            <p class="title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛阿斯顿发 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="subtitle article-date">
+              <span>2017-06-28 17:56</span>
+              <span> | </span>
+              <span>11111/11111</span>
+            </p>
+          </router-link>
+          <router-link class="article" :to="{name: 'detail', params:{ postId: 1}}" tag="div">
+            <p class="title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛阿斯顿发 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="subtitle article-date">
+              <span>2017-06-28 17:56</span>
+              <span> | </span>
+              <span>11111/11111</span>
+            </p>
+          </router-link>
+          <router-link class="article" :to="{name: 'detail', params:{ postId: 1}}" tag="div">
+            <p class="title is-ellipsis">
+              <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛阿斯顿发 EDG vs RNG 3:2 胜利？
+            </p>
+            <p class="subtitle article-date">
+              <span>2017-06-28 17:56</span>
+              <span> | </span>
+              <span>11111/11111</span>
+            </p>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

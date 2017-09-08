ac9df08542614ffe452fcba337d010153e2f6a6c
zhongxb <zhongxb@firevale.com>
2017-09-08 15:23:02 +0800

    wip

---
+++ lib/password.ex
@@ -0,0 +1,22 @@
+defmodule Utils.Password do 
+
+	def hash(password) do 
+		Pbkdf2.hashpwsalt(password)
+	end
+
+	def check(password, hash) do 
+		case hash do 
+      nil ->
+        false
+			"$pbkdf2-sha512$" <> _ ->
+				Pbkdf2.checkpw(password, hash)
+			_ ->
+				(Utils.encrypt(password) |> String.downcase) == (hash |> String.downcase)
+		end
+  end
+
+  defp encrypt(password) do
+    {:ok, e} = :pbkdf2.pbkdf2(:sha, password, "gGshJCGHNVVltq9n+Bji6w==", 4096, 20)
+    e |> to_hex
+  end
+end

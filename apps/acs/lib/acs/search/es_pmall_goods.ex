bd52c75e038bf5e73466cb60a8fc5b6a9fc7f798
zhongxb <zhongxb@firevale.com>
2017-09-11 10:15:02 +0800

    wip

--- apps/acs/lib/acs/search/es_pmall_goods.ex
+++ apps/acs/lib/acs/search/es_pmall_goods.ex
@@ -4,7 +4,19 @@ defmodule Acs.Search.ESPMallGoods do
 
   def index(%PMallGoods{} = goods) do 
     
-    # todo 
+    Elasticsearch.index(%{
+      index: "pmall",
+      type: "goods",
+      doc: %{
+        id: goods.id,
+        name: goods.name,
+        app_id: goods.app_id,
+        description: goods.description,
+        inserted_at: Timex.format!(goods.inserted_at, "{YYYY}-{0M}-{0D}T{h24}:{0m}:{0s}+00:00"),
+      },
+      params: nil,
+      id: goods.id
+    })
     
   end
 

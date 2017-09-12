7784039d9fe03e4f9a5a599ee595bf1977cdddcc
Minzz <zhumingzhen@firevale.com>
2017-09-12 11:57:19 +0800

    wip

---
+++ static/forum/views/mobile/replyPostModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <modal ref="modal" @deactive="deactive">
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title title is-5 is-ellipsis">
+          <strong>[综合讨论]</strong> 如何评价 LPL 夏季总决赛总决赛 EDG vs RNG 3:2 胜利？
+        </p>
+      </header>
+      <section class="modal-card-body">
+        <quill-editor class="editor" v-model.trim="content" placeholder="请填写帖子内容" @ready="setEditor">
+        </quill-editor>
+      </section>
+      <footer class="modal-card-foot is-centred">
+        <a class="button is-grey is-medium">
+                取消
+        </a>
+        <a class="button is-primary is-medium">
+                回帖
+        </a>
+      </footer>
+    </div>
+  </modal>
+</template>
+<script>
+import {
+  Modal
+} from 'common/components/vue-bulma-modal'
+
+export default {
+  props: {
+
+  },
+  data() {
+    return {
+      content: '',
+      editor: undefined,
+    }
+  },
+
+  methods: {
+    setEditor: function(editor) {
+      this.editor = editor
+    },
+    show() {
+      this.$refs.modal.active()
+    },
+    deactive() {
+      this.visible = false;
+    }
+  },
+
+  components: {
+    Modal,
+  },
+}
+</script>
+<style lang="scss">
+.ql-container {
+  min-height: 20rem;
+}
+</style>

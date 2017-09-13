16f4a667cd3e316755e90f3e004acfcf5c46a919
xiaobin <xiaobin@firevale.com>
2017-09-13 11:49:29 +0800

    wip

--- apps/acs_web/lib/acs_web/controllers/mall/mall_order_controller.ex
+++ apps/acs_web/lib/acs_web/controllers/mall/mall_order_controller.ex
@@ -1,6 +1,8 @@
 defmodule AcsWeb.MallOrderController do
   use AcsWeb, :controller
 
+  alias Acs.Malls
+
   plug :fetch_app_id
   plug :fetch_access_token
   plug :fetch_session_user_id
@@ -49,23 +51,8 @@ defmodule AcsWeb.MallOrderController do
   end
 
   def fetch_order(conn, %{"order_id" => order_id }) do
-    order = fetch_order_info(order_id)
+    order = Malls.get_order_info(order_id)
     json(conn, %{success: true, order: order})
-  end
-
-  defp fetch_order_info( order_id) do
-    query = 
-      from o in MallOrder,
-        left_join: details in assoc(o, :details),
-        left_join: user in assoc(o, :user),
-        left_join: op_logs in assoc(o, :op_logs),
-        select: map(o, [:id, :goods_name, :status, :price, :final_price, :address, :currency, :paid_type, :transaction_id, :postage, :inserted_at, :snapshots,
-          user: [:id, :nickname, :mobile, :email],
-          details: [:id, :goods_name, :goods_pic, :price, :amount, :mall_goods_id],
-          op_logs: [:id, :status, :changed_status, :content, :admin_user, :inserted_at] ]),
-        where: o.id ==^order_id,
-        preload: [user: user, details: details, op_logs: op_logs]
-    Repo.one(query)
   end
 
   # create_mall_order

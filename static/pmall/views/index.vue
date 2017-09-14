4d90a1612fd7b72e038e19f8a595a6904df009cd
Minzz <zhumingzhen@firevale.com>
2017-09-14 16:04:55 +0800

    wip

--- static/pmall/views/index.vue
+++ static/pmall/views/index.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="index-page">
-    这是首页
+    <header class="header is-flex">
+      <div class="header-left is-flex flex-left flex-vcentered">
+        <img src="~assets/pmall/1242-2234-1_13.png">
+        <p>
+          我的积分
+          <br/> 100
+        </p>
+      </div>
+      <div class="header-right is-flex is-column flex-center">
+        <a class="button btn-point"></a>
+        <a class="button btn-my-conversion"></a>
+      </div>
+    </header>
+    <div class="task-panel">
+      <div class="header-title">
+        做任务拿积分
+      </div>
+      <div class="tasks">
+        <div class="task is-flex is-column flex-center flex-vcentered">
+          <img src="~assets/pmall/1242-2234-1_13.png" />
+          <p class="is-marginless">绑定手机</p>
+          <p class="is-marginless">
+            <span>+99</span>积分 限1次</p>
+        </div>
+        <div class="task">
+        </div>
+        <div class="task">
+        </div>
+        <div class="task">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>

9a45e960ec102b439d4dccef02d31f3b7ceba83b
xiebing <xiebing@firevale.com>
2017-09-14 16:15:31 +0800

    wip

---
+++ static/pmall/views/myConversion.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-conversion">
+    <div class="conversion-head is-flex flex-vcentered flex-left">
+      客服邮箱：<i class="icon icon-email"></i>&nbsp;jqxs@firevale.com
+    </div>
+    <div class="conversion-content">
+      <div class="conversion-item" v-for="n in 15">
+        <div>兑换记录1：礼品名称cdkey（休闲鞋）</div>
+        <div>
+          <span>兑换时间：2017年10月11日13时15分</span>
+          <span class="is-right">-45积分</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

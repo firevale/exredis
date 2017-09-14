4be0bedb14ead6607a5086e6e1b77d5fbcda823d
Minzz <zhumingzhen@firevale.com>
2017-09-14 14:11:11 +0800

    wip

--- static/pmall/routes/index.js
+++ static/pmall/routes/index.js
@@ -6,10 +6,41 @@ export default function(VueRouter) {
       name: 'pmall',
       component: require('../views/main.vue'),
       children: [{
-        path: 'index',
-        name: 'index',
-        component: require('../views/index.vue')
-      }]
+          path: 'index',
+          name: 'index',
+          component: require('../views/index.vue')
+        }, {
+          path: 'draw',
+          name: 'draw',
+          component: require('../views/index.vue')
+        },
+        {
+          path: 'my_conversion',
+          name: 'my_conversion',
+          component: require('../views/index.vue')
+        },
+        {
+          path: 'new_address',
+          name: 'new_address',
+          component: require('../views/index.vue')
+        }, {
+          path: 'sign',
+          name: 'sign',
+          component: require('../views/index.vue')
+        }, {
+          path: 'knowledge',
+          name: 'knowledge',
+          component: require('../views/index.vue')
+        }, {
+          path: 'my_point',
+          name: 'my_point',
+          component: require('../views/index.vue')
+        }, {
+          path: 'bind_mobile',
+          name: 'bind_mobile',
+          component: require('../views/index.vue')
+        }
+      ]
     }, {
       path: '/pmall/:appId/error',
       name: 'error',

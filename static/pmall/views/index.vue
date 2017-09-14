53d3b4d6f0af154acffc8daf7810c0dd99dc038c
Minzz <zhumingzhen@firevale.com>
2017-09-14 16:16:42 +0800

    wip

--- static/pmall/views/index.vue
+++ static/pmall/views/index.vue
@@ -19,18 +19,31 @@
       </div>
       <div class="tasks">
         <div class="task is-flex is-column flex-center flex-vcentered">
-          <img src="~assets/pmall/1242-2234-1_13.png" />
+          <img src="~assets/pmall/1242-2234-1_11.png" />
           <p class="is-marginless">绑定手机</p>
           <p class="is-marginless">
-            <span>+99</span>积分 限1次</p>
+            <span class="is-primary">+99</span>积分 限1次</p>
         </div>
-        <div class="task">
+        <div class="task is-flex is-column flex-center flex-vcentered">
+          <img src="~assets/pmall/1242-2234-1_13.png" />
+          <p class="is-marginless">官网预约</p>
+          <p class="is-marginless">
+            <span class="is-primary">+5</span>积分/日</p>
         </div>
-        <div class="task">
+        <div class="task is-flex is-column flex-center flex-vcentered">
+          <img src="~assets/pmall/1242-2234-1_15.png" />
+          <p class="is-marginless">每日签到</p>
+          <p class="is-marginless">
+            <span class="is-primary">+5</span>积分/日</p>
         </div>
-        <div class="task">
+        <div class="task is-flex is-column flex-center flex-vcentered">
+          <img src="~assets/pmall/1242-2234-1_21.png" />
+          <p class="is-marginless">每日问答</p>
+          <p class="is-marginless">
+            <span class="is-primary">+5</span>积分/日</p>
         </div>
       </div>
+      <div class="banner bg-full"></div>
     </div>
   </div>
 </template>

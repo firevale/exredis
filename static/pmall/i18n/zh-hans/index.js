85863bc828dbb0e51caf01fceccd7bf805d2712d
Minzz <zhumingzhen@firevale.com>
2017-09-14 14:00:04 +0800

    wip

--- static/pmall/i18n/zh-hans/index.js
+++ static/pmall/i18n/zh-hans/index.js
@@ -1,11 +1,12 @@
-import forum from './forum'
 import upload from 'common/i18n/zh-hant/upload'
 import common from 'common/i18n/zh-hant/common'
 import error from 'common/i18n/zh-hant/error'
 
 export default {
-  forum,
   upload,
   common,
-  error
+  error,
+  pmall: {
+
+  }
 }

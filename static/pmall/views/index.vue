413990e9732f86f764d30fab123ecfcb4001fd76
Minzz <zhumingzhen@firevale.com>
2017-09-15 10:22:16 +0800

    wip

--- static/pmall/views/index.vue
+++ static/pmall/views/index.vue
@@ -44,7 +44,7 @@
         </div>
       </div>
     </div>
-    <div class="banner bg-full"></div>
+    <div class="banner bg-full flex-fixed-size"></div>
     <div class="panel">
       <div class="header-title">
         做任务拿积分
@@ -54,11 +54,11 @@
           <div class="item-box">
             <div class="item">
               <div class="image"></div>
-              <div class="item-content">
-                <p class="is-flex is-vcentered">
-                  <span class="is-size-4 is-danger">惊奇雨伞</span> <a class="button btn-conversion" style="margin-left:1rem"></a></p>
-                <p>兑换积分:
-                  <span class="is-primary">98</span> <a href="#">查看礼品详情</a>
+              <div class="item-content is-flex is-column is-center">
+                <h1 class="is-size-4 is-danger is-flex flex-vcentered flex-center">
+                  <span class="item-title is-ellipsis">惊奇避孕套</span> <a class="button btn-conversion" style="margin-left:1rem"></a></h1>
+                <p class="is-marginless is-size-6 has-text-centered">兑换积分:
+                  <span class="is-primary">98</span> <a href="#" style="margin-left:1rem">查看礼品详情</a>
                 </p>
               </div>
             </div>
@@ -68,12 +68,34 @@
           <div class="item-box">
             <div class="item">
               <div class="image"></div>
+              <div class="item-content is-flex is-column is-center">
+                <h1 class="is-size-4 is-danger is-flex flex-vcentered flex-center">
+                      <span class="item-title is-ellipsis">惊奇雨伞</span> <a class="button btn-conversion" style="margin-left:1rem"></a></h1>
+                <p class="is-marginless is-size-6 has-text-centered">兑换积分:
+                  <span class="is-primary">98</span> <a href="#" style="margin-left:1rem">查看礼品详情</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
         <div class="column is-6">
           <div class="item-box">
             <div class="item">
+              <div class="image"></div>
+              <div class="item-content is-flex is-column is-center">
+                <h1 class="is-size-4 is-danger is-flex flex-vcentered flex-center">
+                      <span class="item-title is-ellipsis">惊奇抱枕</span> <a class="button btn-conversion" style="margin-left:1rem"></a></h1>
+                <p class="is-marginless is-size-6 has-text-centered">兑换积分:
+                  <span class="is-primary">98</span> <a href="#" style="margin-left:1rem">查看礼品详情</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column is-6">
+          <div class="item-box">
+            <div class="item is-flex flex-center flex-vcentered">
+              <a class="button btn-more-goods" />
             </div>
           </div>
         </div>

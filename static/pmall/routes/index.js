874a4a4db751916942b489ce45e3cbce21254b20
Minzz <zhumingzhen@firevale.com>
2017-09-15 11:45:11 +0800

    wip

--- static/pmall/routes/index.js
+++ static/pmall/routes/index.js
@@ -10,6 +10,10 @@ export default function(VueRouter) {
           name: 'index',
           component: require('../views/index.vue')
         }, {
+          path: 'pmall',
+          name: 'pmall',
+          component: require('../views/pmall.vue')
+        }, {
           path: 'draw',
           name: 'draw',
           component: require('../views/draw.vue')
@@ -34,7 +38,7 @@ export default function(VueRouter) {
         }, {
           path: 'my_point',
           name: 'my_point',
-          component: require('../views/index.vue')
+          component: require('../views/myPoint.vue')
         }, {
           path: 'bind_mobile',
           name: 'bind_mobile',

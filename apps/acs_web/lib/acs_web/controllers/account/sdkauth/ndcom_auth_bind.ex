9a90665a306ec31985ec4c44f547bc76c0125480
xiaobin <xiaobin@firevale.com>
2017-09-18 15:22:24 +0800

    wip

--- apps/acs_web/lib/acs_web/controllers/account/sdkauth/ndcom_auth_bind.ex
+++ apps/acs_web/lib/acs_web/controllers/account/sdkauth/ndcom_auth_bind.ex
@@ -5,48 +5,44 @@ defmodule AcsWeb.NdcomAuthBind do
 
   def bind(%Plug.Conn{
               private: %{
-                acs_app: %App{sdk_bindings: %{ndcom: %{"app_id" => ndcom_app_id, "app_key" => ndcom_app_key}}} = app,
+                acs_app: %App{} = app,
                 acs_device_id: device_id,
                 acs_platform: platform}} = conn, 
             %{"ndcom_session_id" => ndcom_session_id,
               "ndcom_user_id" => ndcom_user_id} = params) do
-
-    if SDKNdcom.validate_session(ndcom_app_id, ndcom_app_key, ndcom_session_id, ndcom_user_id) do
-      case Accounts.bind_sdk_user(%{sdk: :ndcom, 
-                                     app_id: app.id, 
-                                     sdk_user_id: ndcom_user_id, 
-                                     email: nil,
-                                     nickname: params["ndcom_nickname"],
-                                     device_id: device_id,
-                                     mobile: nil,
-                                     avatar_url: nil}) do 
-        {:ok, user} -> 
-          access_token = Auth.create_access_token(%{
-            app_id: app.id,
-            user_id: user.id,
-            device_id: device_id,
-            platform: platform,
-            ttl: app.token_ttl,
-            binding: %{ndcom: %{session_id: ndcom_session_id, user_id: ndcom_user_id}}
-          })
-
-          conn |> json(%{
-            success: true,
-            access_token: access_token.id,
-            expires_at: AccessToken.expired_at(access_token),
-            user_id: "#{user.id}",
-            user_email: user.email,
-            nick_name:  user.nickname,
-            is_anonymous: false,
-            sdk: :ndcom,
-            binding: access_token.binding              
-          })
-
-        _ ->
-          conn |> json(%{success: false, message: "can't bind sdk user"})
-      end
-    else 
-      conn |> json(%{success: false, message: "validate session failed"})
+    with %AppSdkBinding{binding: %{
+           "app_id" => ndcom_app_id, 
+           "app_key" => ndcom_app_key}} <- Apps.get_app_sdk_binding(app.id, "ndcom"),
+         true <- SDKNdcom.validate_session(ndcom_app_id, ndcom_app_key, ndcom_session_id, ndcom_user_id),
+         {:ok, user} <- Accounts.bind_sdk_user(%{
+           sdk: :ndcom, 
+           sdk_user_id: ndcom_user_id, 
+           email: nil,
+           mobile: nil, 
+           }),
+         access_token <- Auth.create_access_token(%{
+           app_id: app.id,
+           user_id: user.id,
+           device_id: device_id,
+           platform: platform,
+           ttl: app.token_ttl,
+           binding: %{ndcom: %{session_id: ndcom_session_id, user_id: ndcom_user_id}}
+         })
+    do 
+      conn |> json(%{
+        success: true,
+        access_token: access_token.id,
+        expires_at: AccessToken.expired_at(access_token),
+        user_id: "#{user.id}",
+        user_email: user.email,
+        nick_name:  user.nickname,
+        is_anonymous: false,
+        sdk: :ndcom,
+        binding: access_token.binding              
+      })
+    else
+      _ ->
+        conn |> json(%{success: false, message: "something went wrong"}) 
     end
   end 
   def bind(conn, _params), do: conn |> json(%{success: false, message: "invalid request"})

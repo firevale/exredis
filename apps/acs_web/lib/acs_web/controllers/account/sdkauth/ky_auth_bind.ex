f6a0d1e23cef2ce93df3d11b184fbb7d34d08dd9
xiaobin <xiaobin@firevale.com>
2017-09-18 14:54:19 +0800

    wip

--- apps/acs_web/lib/acs_web/controllers/account/sdkauth/ky_auth_bind.ex
+++ apps/acs_web/lib/acs_web/controllers/account/sdkauth/ky_auth_bind.ex
@@ -1,52 +1,45 @@
 defmodule AcsWeb.KYAuthBind do
   use     AcsWeb, :controller
-  require Logger
   require SDKKY
+  alias   Acs.Apps
 
   def bind(%Plug.Conn{
               private: %{
-                acs_app: %App{sdk_bindings: %{ky: %{"app_key" => ky_app_key}}} = app,
+                acs_app: %App{} = app,
                 acs_device_id: device_id,
                 acs_platform: platform}} = conn, 
-            %{"ky_session_id" => ky_session_id} = _params) do
-
-    case SDKKY.validate_session(ky_app_key, ky_session_id) do
-      %{id: ky_user_id, nickname: ky_nickname} ->
-        case Accounts.bind_sdk_user(%{sdk: :ky, 
-                                       app_id: app.id, 
-                                       sdk_user_id: ky_user_id, 
-                                       email: nil,
-                                       nickname: ky_nickname,
-                                       device_id: device_id,
-                                       mobile: nil,
-                                       avatar_url: nil}) do 
-          {:ok, user} -> 
-            access_token = Auth.create_access_token(%{
-              app_id: app.id,
-              user_id: user.id,
-              device_id: device_id,
-              platform: platform,
-              ttl: app.token_ttl,
-              binding: %{ky: %{session_id: ky_session_id, user_id: ky_user_id}}
-            })
-
-            conn |> json(%{
-              success: true,
-              access_token: access_token.id,
-              expires_at: AccessToken.expired_at(access_token),
-              user_id: "#{user.id}",
-              user_email: user.email,
-              nick_name:  user.nickname,
-              is_anonymous: false,
-              sdk: :ky,
-              binding: access_token.binding              
-            })
-
-          _ ->
-            conn |> json(%{success: false, message: "can't bind sdk user"})
-        end
+            %{"ky_session_id" => ky_session_id}) do
+    with %AppSdkBinding{binding: %{"app_key" => ky_app_key}} <- Apps.get_app_sdk_binding(app.id, "ky"),
+         %{id: ky_user_id, nickname: ky_nickname} <- SDKKY.validate_session(ky_app_key, ky_session_id),
+         {:ok, user} <- Accounts.bind_sdk_user(%{
+           sdk: :ky, 
+           sdk_user_id: ky_user_id, 
+           email: nil,
+           mobile: nil, 
+           }),
+         access_token <- Auth.create_access_token(%{
+           app_id: app.id,
+           user_id: user.id,
+           device_id: device_id,
+           platform: platform,
+           ttl: app.token_ttl,
+           binding: %{ky: %{session_id: ky_session_id, user_id: ky_user_id}}
+         })
+    do 
+      conn |> json(%{
+        success: true,
+        access_token: access_token.id,
+        expires_at: AccessToken.expired_at(access_token),
+        user_id: "#{user.id}",
+        user_email: user.email,
+        nick_name:  user.nickname,
+        is_anonymous: false,
+        sdk: :ky,
+        binding: access_token.binding              
+      })
+    else
       _ ->
-        conn |> json(%{success: false, message: "validate session failed"})
+        conn |> json(%{success: false, message: "something went wrong"}) 
     end
   end 
   def bind(conn, _params), do: conn |> json(%{success: false, message: "invalid request"})

db08620f55909c76a7dc0070b1a7f734d1bc7260
zhongxb <zhongxb@firevale.com>
2017-09-19 09:32:02 +0800

    wip

--- static/admin/i18n/zh-hans/admin/point.js
+++ static/admin/i18n/zh-hans/admin/point.js
@@ -10,9 +10,24 @@
 
    task: {
      add: '添加新任务',
+     edit: '编辑',
+     delete: '删除',
      taskbar: '任务栏',
      addSuccess: '任务添加成功',
      updateSuccess: '任务修改成功',
      taskNotFound: '任务不存在',
+     id: '编号',
+     pic: '题图',
+     name: '标题',
+     sub_name: '子标题',
+     point: '积分',
+     path: '路径',
+     active: '启用',
+     sort: '排序',
+     publishOk: '发布成功',
+     unPublishOK: '取消发布成功',
+     confirmDeleteTask: '您确定要删除该任务栏么?',
+     configUpdated: '任务栏已成功更新',
+     configDeleted: '任务栏已成功删除',
    }
  }

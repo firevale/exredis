e343f800bc788ba99e24d35368905583826ba08e
xiaobin <xiaobin@firevale.com>
2017-09-20 13:45:25 +0800

    wip

--- config/ksbj.exs
+++ config/ksbj.exs
@@ -4,7 +4,7 @@ import_config "prod_base.exs"
 
 config :acs_web, AcsWeb.Endpoint,
   url: [host: "fvac.firevale.com", port: 443, scheme: "https"],
-  static_url: [host: "acs01.firevale.com", port: 443, scheme: "https", path: "acs"]
+  static_url: [host: "acs01.firevale.com", port: 443, scheme: "https", path: "/acs"]
 
 config :exsm, :provider, :meisheng
 

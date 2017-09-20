0d56d8e40dd06678040022f601f406b0563d578f
xiaobin <xiaobin@firevale.com>
2017-09-20 20:11:04 +0800

    wip

--- config/ksbj.exs
+++ config/ksbj.exs
@@ -2,6 +2,8 @@ use Mix.Config
 
 import_config "prod_base.exs"
 
+config :acs, :location, "cn"
+
 config :acs_web, AcsWeb.Endpoint,
   url: [host: "fvac.firevale.com", port: 443, scheme: "https"],
   static_url: [host: "acs01.firevale.com", port: 443, scheme: "https", path: "/acs"]

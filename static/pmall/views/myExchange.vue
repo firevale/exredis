66edd6c5da65886b486390c68809c95d2dfece28
Minzz <zhumingzhen@firevale.com>
2017-09-21 14:28:18 +0800

    wip

--- static/pmall/views/myExchange.vue
+++ static/pmall/views/myExchange.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="my-conversion">
-    <scroller :on-load-more="loadmore" ref="scroller">
-      <header class="is-flex flex-vcentered flex-left is-size-medium">
-        客服邮箱：<i class="icon icon-email"></i>&nbsp;jqxs@firevale.com
-      </header>
-      <div class="conversion-content">
-        <div v-for="item in point_logs" class="conversion-item">
-          <span>{{item.memo}}</span>
-          <div>
-            <span class="is-size-6 is-grey">兑换时间：{{item.inserted_at | formatServerDateTime}}</span>
-            <span class="is-right">{{item.point}}积分</span>
-          </div>
+    <header class="is-flex flex-vcentered flex-left is-size-medium">
+      客服邮箱：<i class="icon icon-email"></i>&nbsp;jqxs@firevale.com
+    </header>
+    <div class="conversion-content">
+      <div v-for="item in point_logs" class="conversion-item">
+        <span>{{item.memo}}</span>
+        <div>
+          <span class="is-size-6 is-grey">兑换时间：{{item.inserted_at | formatServerDateTime}}</span>
+          <span class="is-right">{{item.point}}积分</span>
         </div>
-        <!-- <footer class="is-flex flex-center flex-vcentered">
-          <div class="loadmore">查看更多</div>
-        </footer> -->
       </div>
-    </scroller>
+      <footer v-show="this.page!=0 && this.total>this.page" class="is-flex flex-center flex-vcentered">
+        <div class="loadmore" @click="loadData">查看更多</div>
+      </footer>
+    </div>
   </div>
 </template>
 <script>
@@ -29,15 +27,10 @@ export default {
     }
   },
   mounted() {
-    this.loadmore()
+    this.loadData()
   },
   methods: {
-    resetScroller: function() {
-      if (this.$refs.scroller) {
-        this.$refs.scroller.$emit('reset')
-      }
-    },
-    loadmore: async function() {
+    loadData: async function() {
       this.$acs.cancel('listMyPoints')
       let result = await this.$acs.listMyPoints({
         page: this.page + 1,
@@ -46,11 +39,7 @@ export default {
       if (result.success) {
         this.point_logs = this.point_logs.concat(result.point_logs)
         this.total = result.total
-        this.page = this.page + 1
-
-        if (this.$refs.scroller && this.page >= this.total) {
-          this.$refs.scroller.$emit('all-loaded')
-        }
+        this.page++
       }
     }
   }

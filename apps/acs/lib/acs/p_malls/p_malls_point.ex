4612815002bbec11afab206b16baeda8ea07bbdf
Minzz <zhumingzhen@firevale.com>
2017-09-21 18:33:50 +0800

    wip

--- apps/acs/lib/acs/p_malls/p_malls_point.ex
+++ apps/acs/lib/acs/p_malls/p_malls_point.ex
@@ -15,18 +15,18 @@ defmodule Acs.PMallsPoint do
   alias Acs.Cache.CachedAdminSetting
 
   def add_point(log_type, app_id, wcp_user_id) do
-    point = 5
-    memo = "积分测试"
+    %Acs.Admin.Setting{} = setting  = CachedAdminSetting.get_fat(app_id, log_type)
     
     log = %{
       app_id: app_id,
       wcp_user_id: wcp_user_id,
       log_type: log_type,
-      point: point,
-      memo: memo
+      point: setting.value,
+      memo: setting.memo
     }
-   
+
     PMalls.add_point(log)
+
   end
 
 end

dfc7bc2f192fbed16711e3192fa5e47f02988d60
xiaobin <xiaobin@firevale.com>
2017-09-22 19:52:19 +0800

    wip

--- static/admin/views/app/dashboard.vue
+++ static/admin/views/app/dashboard.vue
@@ -34,7 +34,7 @@
         </div>
       </div>
     </nav>
-    <nav class="level">
+    <!-- <nav class="level">
       <div class="level-item has-text-centered box">
         <div>
           <p class="heading">{{ $t('admin.app.dau') }}</p>
@@ -67,7 +67,7 @@
           </p>
         </div>
       </div>
-    </nav>
+    </nav> -->
     <article class="tile box chart">
       <line-chart ref="chart" :options="options" :width="'100%'"></line-chart>
     </article>

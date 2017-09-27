83b64b2be6a04810a96ce55bcedf68ac36df7055
xiaobin <xiaobin@firevale.com>
2017-09-27 17:46:58 +0800

    wip

--- apps/acs_web/lib/acs_web/controllers/pmall/pmall_controller.ex
+++ apps/acs_web/lib/acs_web/controllers/pmall/pmall_controller.ex
@@ -6,9 +6,7 @@ defmodule AcsWeb.PMallController do
   alias Acs.PMallsPoint
 
   plug :fetch_app_id
-  plug :fetch_access_token
-  plug :fetch_session_user_id
-  plug :fetch_session_user
+  plug :fetch_session_wcp_user_id
 
   def list_index(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, params) do
     #任务清单
@@ -29,7 +27,7 @@ defmodule AcsWeb.PMallController do
   # list_pmall_goods
   def list_goods(%Plug.Conn{private: %{acs_app_id: app_id}} = conn,
                                       %{"page" => page,
-                                      "records_per_page" => records_per_page}) do
+                                        "records_per_page" => records_per_page}) do
     {total_page, goodses} =
       case PMalls.list_pmall_goods(app_id, page, records_per_page, "") do
         {:ok, goodses, total_page} ->
@@ -41,21 +39,17 @@ defmodule AcsWeb.PMallController do
     conn |> json(%{success: true, total: total_page, goodses: goodses})
   end
 
-  def get_goods_detail(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"goods_id" =>goods_id})do
+  def get_goods_detail(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, %{"goods_id" =>goods_id})do
     PMalls.add_goods_click(goods_id, 1)
     goods = PMalls.get_pmall_goods_detail(goods_id)
-
-    wcp_user_id = 1
     exchange_count =  PMalls.count_exchange_goods(app_id, wcp_user_id, goods_id)
-    conn |> json(%{success: true, goods: goods, exchanged: exchange_count>=1})
+    conn |> json(%{success: true, goods: goods, exchanged: exchange_count >= 1})
   end
 
-  def get_user_info(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, _) do
+  def get_user_info(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, _) do
     # user = Accounts.get_user()
-    wcp_user_id = 1
     point = PMalls.get_user_point(app_id, wcp_user_id)
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-    case CachedAppWcpUser.get(app_id, open_id) do
+    case Wcp.get_app_wcp_user(wcp_user_id) do
       %AppWcpUser{} = user ->
         wcp_user = Map.take(user, [:id, :nickname, :avatar_url, :app_id])
         user_info = Map.merge(wcp_user, %{points: point, has_mobile: false })
@@ -65,11 +59,9 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def list_my_points(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"page" => page,
-  "records_per_page" => records_per_page}) do
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-    wcp_user_id = 1
-
+  def list_my_points(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, 
+    %{"page" => page,
+      "records_per_page" => records_per_page}) do
     result = PMalls.list_my_points(app_id, wcp_user_id, page, records_per_page)
     case result do
       {:ok, point_logs, total} ->
@@ -79,11 +71,9 @@ defmodule AcsWeb.PMallController do
      end
   end
 
-  def list_my_exchanges(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"page" => page,
-  "records_per_page" => records_per_page}) do
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-    wcp_user_id = 1
-
+  def list_my_exchanges(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, 
+    %{"page" => page,
+      "records_per_page" => records_per_page}) do
     result = PMalls.list_my_exchanges(app_id, wcp_user_id, page, records_per_page)
     case result do
       {:ok, point_logs, total} ->
@@ -93,8 +83,7 @@ defmodule AcsWeb.PMallController do
      end
   end
 
-  def exchange(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"goods_id" => goods_id}) do
-    wcp_user_id = 1
+  def exchange(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, %{"goods_id" => goods_id}) do
     result = PMalls.exchange_goods(app_id, wcp_user_id, goods_id)
     case result do
       {:ok, exchange_info} ->
@@ -104,8 +93,7 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def take_award(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"days" => days}) do
-    wcp_user_id = 1
+  def take_award(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, %{"days" => days}) do
     result = PMalls.take_sign_award(app_id, wcp_user_id, days)
     case result do
       {:ok, add_point, total_point} ->
@@ -116,7 +104,7 @@ defmodule AcsWeb.PMallController do
 
   end
 
-  def update_address(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, 
+  def update_address(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, 
     %{"order_id" => order_id,  
       "user_address" => 
         %{"name" => name,
@@ -124,9 +112,7 @@ defmodule AcsWeb.PMallController do
           "area" => area, 
           "address" => address, 
           "area_code" => area_code} = user_address}) do
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_EE"
-    wcp_user_id  = 1
-    result = PMalls.update_order_address( wcp_user_id, order_id, user_address)
+    result = PMalls.update_order_address(wcp_user_id, order_id, user_address)
     case result do
       {:ok, order} ->
         conn |> json(%{success: true})
@@ -135,36 +121,41 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def get_sign_info(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, params) do
-    wcp_user_id = 1
+  def get_sign_info(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, params) do
     {:ok, signed, sign_times, pic_raw, terms, awards} = PMalls.get_sign_info(app_id, wcp_user_id)
     %{"pic" => pic} = pic_raw
     {total, sign_users} = PMalls.get_sign_users(app_id)
-    conn |> json(%{success: true, signed: signed, sign_times: sign_times, pic: pic, terms: terms,
-    awards: awards, sign_total: total, sign_users: sign_users})
+    conn |> json(%{
+      success: true, 
+      signed: signed, 
+      sign_times: sign_times, 
+      pic: pic, 
+      terms: terms,
+      awards: awards, 
+      sign_total: total, 
+      sign_users: sign_users})
   end
 
-  def sign(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, params) do
-    wcp_user_id = 1
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-
+  def sign(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, params) do
+    wcp_user = Wcp.get_app_wcp_user(wcp_user_id)
     result = PMalls.sign(app_id, wcp_user_id)
+
     case result do
       {:ok, sign_info} ->
-        PMalls.add_sign_user(app_id, open_id)
+        PMalls.add_sign_user(app_id, wcp_user.openid)
         conn |> json(Map.merge(%{success: true}, sign_info))
+
       {:error, %{i18n_message: i18n_message}} ->
         conn |> json(%{success: false, i18n_message: i18n_message})
     end
   end
 
-  def bind_mobile(conn, %{"mobile" => mobile, "verify_code" => verify_code}) do
-    app_id = "3E4125B15C4FE2AB3BA00CB1DC1A0EE5"
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-
+  def bind_mobile(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, 
+    %{"mobile" => mobile, "verify_code" => verify_code}) do
+    wcp_user = Wcp.get_app_wcp_user(wcp_user_id)
     case get_session(conn, :bind_mobile_verify_code) do
       ^verify_code ->
-        case PMalls.bind_mobile(app_id, open_id, mobile) do
+        case PMalls.bind_mobile(app_id, wcp_user.openid, mobile) do
           {:ok, mobile} ->
             conn |> delete_session(:bind_mobile_verify_code)
                  |> json(Map.merge(%{success: true}, mobile))
@@ -176,15 +167,14 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def insert_address(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{
-                "name" => _name,
-                "mobile" => _mobile,
-                "area" => _area,
-                "address" => _address,
-                "area_code" => _area_code} = us_address) do
+  def insert_address(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, %{
+    "name" => _name,
+    "mobile" => _mobile,
+    "area" => _area,
+    "address" => _address,
+    "area_code" => _area_code} = us_address) do
 
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_WA"
-    case Wcp.get_app_wcp_user(app_id, openid: open_id) do
+    case Wcp.get_app_wcp_user(wcp_user_id) do
       nil ->
         conn |> json(%{success: false, message: "invalid request params"})
 
@@ -198,8 +188,7 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def get_daily_question(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, _) do
-    wcp_user_id = 1
+  def get_daily_question(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, _) do
     exists = PMalls.exists_answer_user(app_id, wcp_user_id)
     case PMalls.get_daily_question(app_id) do
       nil ->
@@ -209,9 +198,8 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def answer_question(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, %{"id" => id,
+  def answer_question(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, %{"id" => id,
     "correct" => correct}) do
-      wcp_user_id = 1
       result = PMalls.answer_question(id, app_id, wcp_user_id, correct)
       case result do
         {:ok, answer} ->
@@ -221,8 +209,7 @@ defmodule AcsWeb.PMallController do
       end
   end
 
-  def luck_draw(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, _) do
-    wcp_user_id = 1
+  def luck_draw(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, _) do
     result = PMalls.luck_draw(app_id, wcp_user_id)
     case result do
       {:ok, draw} ->
@@ -232,7 +219,7 @@ defmodule AcsWeb.PMallController do
     end
   end
 
-  def update_draw_address(%Plug.Conn{private: %{acs_app_id: app_id}} = conn, 
+  def update_draw_address(%Plug.Conn{private: %{acs_app_id: app_id, wcp_user_id: wcp_user_id}} = conn, 
     %{
       "order_id" => order_id,  
       "user_address" => 
@@ -241,8 +228,6 @@ defmodule AcsWeb.PMallController do
           "area" => area, 
           "address" => address, 
           "area_code" => area_code} = user_address}) do
-    open_id = "o4tfGszZK1U0c_Z6lj29NAYAv_EE"
-    wcp_user_id  = 1
     result = PMalls.update_draw_address( wcp_user_id, order_id, user_address)
     case result do
       {:ok, order} ->

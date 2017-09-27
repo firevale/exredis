ced7012bdc4f20931937f03a7b10e2619498520a
xiaobin <xiaobin@firevale.com>
2017-09-27 11:58:36 +0800

    wip

--- apps/acs_web/lib/acs_web/controllers/account/sdkauth/ky_auth_bind.ex
+++ apps/acs_web/lib/acs_web/controllers/account/sdkauth/ky_auth_bind.ex
@@ -16,6 +16,7 @@ defmodule AcsWeb.KYAuthBind do
            sdk_user_id: ky_user_id, 
            email: nil,
            mobile: nil, 
+           nickname: nil,
            }),
          access_token <- Auth.create_access_token(%{
            app_id: app.id,

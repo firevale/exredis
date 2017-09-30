10bf09edbf5638c59d683ea74f0d035a91bf5abc
xiaobin <xiaobin@firevale.com>
2017-09-30 19:05:14 +0800

    wip

--- static/admin/views/app/orders.vue
+++ static/admin/views/app/orders.vue
@@ -106,6 +106,10 @@ export default {
     }
   },
 
+  created: async function() {
+    this.keyword = this.$route.params.keyword || ''
+  },
+
   computed: {
     ...mapGetters([
       'goods', 'app'

6a7e4f011ac0531a2d51559281b9af0a04e88378
xiaobin <xiaobin@firevale.com>
2017-10-09 16:54:26 +0800

    git subrepo clone --branch=umbrella ssh://git@gitpub.firevale.com:10022/platform/exmail.git apps/exmail
    subrepo:
      subdir:   "apps/exmail"
      merged:   "83cb5552"
    upstream:
      origin:   "ssh://git@gitpub.firevale.com:10022/platform/exmail.git"
      branch:   "umbrella"
      commit:   "83cb5552"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ apps/exmail/lib/exmail.ex
@@ -0,0 +1,18 @@
+defmodule Exmail do
+  @moduledoc """
+  Documentation for Exmail.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exmail.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

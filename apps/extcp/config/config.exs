6d8c4c0f5f6ecb07d996e749e64da0ad09bf93aa
xiaobin <xiaobin@firevale.com>
2017-10-09 16:36:26 +0800

    wip

--- apps/extcp/config/config.exs
+++ apps/extcp/config/config.exs
@@ -10,11 +10,11 @@ use Mix.Config
 
 # You can configure your application as:
 #
-#     config :exutils, key: :value
+#     config :extcp, key: :value
 #
 # and access this configuration in your application as:
 #
-#     Application.get_env(:exutils, :key)
+#     Application.get_env(:extcp, :key)
 #
 # You can also configure a 3rd-party app:
 #
--- apps/extcp/mix.exs
+++ apps/extcp/mix.exs
@@ -1,9 +1,9 @@
-defmodule Exutils.Mixfile do
+defmodule Extcp.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :exutils,
+      app: :extcp,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
@@ -18,18 +18,15 @@ defmodule Exutils.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :pbkdf2, :comeonin]
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:pbkdf2, "~> 2.0"}, 
-      {:comeonin, "~> 4.0"},
-      {:pbkdf2_elixir, "~> 0.12"},
-      {:poison, "~> 3.1"},
-      {:httpotion, "~> 3.0"},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end

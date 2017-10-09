a818c0f0de7e853b42e24c6f524691f45148ebf8
xiaobin <xiaobin@firevale.com>
2017-10-09 16:52:13 +0800

    git subrepo clone --branch=umbrella ssh://git@gitpub.firevale.com:10022/platform/exutils.git apps/exutils
    subrepo:
      subdir:   "apps/exutils"
      merged:   "d6c23f3f"
    upstream:
      origin:   "ssh://git@gitpub.firevale.com:10022/platform/exutils.git"
      branch:   "umbrella"
      commit:   "d6c23f3f"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ apps/exutils/mix.exs
@@ -0,0 +1,35 @@
+defmodule Exutils.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exutils,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.4",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :pbkdf2, :comeonin]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:pbkdf2, "~> 2.0"}, 
+      {:comeonin, "~> 4.0"},
+      {:pbkdf2_elixir, "~> 0.12"},
+      {:poison, "~> 3.1"},
+      {:httpotion, "~> 3.0"},
+    ]
+  end
+end

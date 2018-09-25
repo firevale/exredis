5d04f6128014f7c56e733a27369165ece48b4e30
徐晓滨 <xbinxu@gmail.com>
2018-09-25 14:30:55 +0800

    using redix 0.7 to avoid pubsub issue

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exredis.Mixfile do
   def project do
     [
       app: :exredis,
-      version: "0.2.1",
+      version: "0.2.4",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
       deps: deps()

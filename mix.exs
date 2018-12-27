a2e4622a2a51b923c78948c671315e6e217bf1ef
徐晓滨 <xbinxu@gmail.com>
2018-12-27 16:09:52 +0800

    wip

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exredis.Mixfile do
   def project do
     [
       app: :exredis,
-      version: "0.9.0",
+      version: "0.8.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps()
